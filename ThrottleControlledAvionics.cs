6aed3369f35941b57cb869e6576099106d1f3370
Willem <willem@firedroid.net>
2014-11-20 17:56:00 +0100

    Toolbar plugin usage replaced with KSP's native toolbar. Gave the GUI it's own class Redone the toolbar icons

--- ThrottleControlledAvionics.cs
+++ ThrottleControlledAvionics.cs
@@ -24,7 +24,7 @@ using System.Text;
 using UnityEngine;
 using KSP.IO;
 using System.IO;
-using Toolbar;
+//using Toolbar;
 
 namespace ThrottleControlledAvionics
 {
@@ -33,53 +33,37 @@ namespace ThrottleControlledAvionics
 	{
 		#region variables
 		
-		private Vessel vessel;
-		protected Rect windowPos;
-		protected Rect windowPosHelp;
+		public Vessel vessel;
 		private bool enginesCounted;
-		private List<EngineWrapper> engineTable;
+		public List<EngineWrapper> engineTable;
 		private Vector3 presentTorque;
-        private Vector3 demand;
-        private float minEfficiency;
-        private Vector3 mainThrustAxis;     // as a pointing vector
-        private enum thrustDirections { up, mean, custom };
-        private thrustDirections direction;
-        private Vector2 customDirectionVector;  // As (p, y)
+        public Vector3 demand;
+		public float minEfficiency;
+		public Vector3 mainThrustAxis;     // as a pointing vector
+        public enum thrustDirections { up, mean, custom };
+        public thrustDirections direction;
+        public Vector2 customDirectionVector;  // As (p, y)
 		
-		private bool isActive;
-		private bool showAny;
-		private bool showEngines;
+		public bool isActive;
 		private bool contUpdate;
-		private bool detectStearingThrusters;
-		private bool reverseThrust;
-		private bool showHelp;
-        private bool showDirection;
-        private bool showDirectionSelector = true;
-        private bool showHUD = true;
-		
-		private IButton TCAButton;
-		private SaveFile save;
-        GUIContent[] saveList;
-        private ComboBox saveListBox;
-        GUIContent[] directionList;
-        private ComboBox directionListBox;
-        private Rect windowDirectionPos;
-        private Vector2 positionScrollViewEngines;
+		public bool detectStearingThrusters;
+		public bool reverseThrust;
+
+		public SaveFile save;     
+		private TCAGui gui;
 		
 		#endregion
 		
 		#region Unity interface
 		
 		public void Awake()  
-		{
-			
+		{			
 			// open save
 			save = new SaveFile();
 			save.Load();
 			// Initialising all variables
 			vessel = null;
-			windowPos = new Rect(50, 50, 400, 200);
-			windowPosHelp = new Rect(500, 100, 400, 50);
+
 			enginesCounted = false;
 			engineTable = new List<EngineWrapper>();
 			presentTorque = new Vector3(0f, 0f, 0f);
@@ -87,38 +71,13 @@ namespace ThrottleControlledAvionics
             minEfficiency = 0.66f;
             mainThrustAxis = Vector3.down;
             direction = thrustDirections.up;
-
-			
+						
 			isActive = false;
-			showAny = false;
-			showEngines = false;
 			contUpdate = true;
-			showHelp = false;
 			detectStearingThrusters = false;
 			reverseThrust = false;
-            showDirection = false;
-			
-			TCAButton = ToolbarManager.Instance.add("TCA", "TCAButton");
-			TCAButton.TexturePath = "ThrottleControlledAvionics/textures/icon_button_off";
-			TCAButton.ToolTip = "Throttle Controlled Avionics";
-			TCAButton.Visibility = new GameScenesVisibility(GameScenes.FLIGHT);
-			TCAButton.Visible = true;
-			TCAButton.OnClick += (e) =>
-			{
-				showAny = !showAny;
-			};
-			
-			saveList = new GUIContent[3];
-			saveList[0] = new GUIContent(save.GetName(0));
-			saveList[1] = new GUIContent(save.GetName(1));
-			saveList[2] = new GUIContent(save.GetName(2));
-			saveListBox = new ComboBox();
 
-            directionList = new GUIContent[3];
-            directionList[0] = new GUIContent("up");
-            directionList[1] = new GUIContent("mean");
-            directionList[2] = new GUIContent("custom");
-            directionListBox = new ComboBox();
+			gui = new TCAGui (this);
 		}
 
 		/// <summary>
@@ -150,6 +109,11 @@ namespace ThrottleControlledAvionics
             }
             
 		}
+
+		public void OnGUI()
+		{
+			gui.OnGUI ();
+		}
 		
 		public void Update() {
             if (Input.GetKeyDown("y"))
@@ -162,258 +126,50 @@ namespace ThrottleControlledAvionics
             //}
             if (Input.GetKeyDown(KeyCode.F2))
             {
-                showHUD = !showHUD;
+                //gui.showHUD = !gui.showHUD; //hmmm... now quite, what is the hud was hidden already when we hit f2?
             }
 		}
 		
-		public void OnGUI()
-		{
-            if (showAny && showHUD) { drawGUI(); }
-            UpdateToolbarIcon();
-		}
+
 		
 		#endregion
 		
 		#region GUI
-		
-		private void drawGUI()
-		{
-			windowPos = GUILayout.Window(1, windowPos, WindowGUI, "Throttle Controlled Avionics");
 
-            if (showHelp) { windowPosHelp = GUILayout.Window(2, windowPosHelp, windowHelp, "Instructions"); }
-            if (showDirection) { DrawDirection(); }
-            if (direction == thrustDirections.custom && showDirectionSelector)
-            {
-                if (windowDirectionPos.xMin <= 1) { windowDirectionPos = new Rect(windowPos.xMax, windowPos.yMin + 50, 120,50); }
-                windowDirectionPos = GUILayout.Window(3, windowDirectionPos, WindowDirectionSelector, "Direction Selector");
-            }
-		}
-
-        private void WindowGUI(int windowID)
-        {
-            if (GUI.Button(new Rect(windowPos.width - 23f, 2f, 20f, 18f), "?"))
-            {
-                showHelp = !showHelp;
-            }
-
-            GUILayout.BeginVertical();
-
-            if (!ElectricChargeAvailible(vessel)) { GUILayout.Label("WARNING! Electric charge has run out!"); }
-
-            isActive = GUILayout.Toggle(isActive, "Toggle TCA");
-
-            GUILayout.BeginHorizontal();
-            GUILayout.Label("Settings: ", GUILayout.ExpandWidth(true));
-            InsertDropdownboxSave();
-            GUILayout.EndHorizontal();
-
-            GUILayout.BeginHorizontal();
-            GUILayout.Label("Sensitivity: ", GUILayout.ExpandWidth(true));
-            GUILayout.Label("" + save.GetActiveSensitivity(), GUILayout.ExpandWidth(false));
-            GUILayout.EndHorizontal();
-            save.SetActiveSensitivity(Mathf.Exp(GUILayout.HorizontalSlider(Mathf.Log(save.GetActiveSensitivity()), -2.0f, 2.0f)));
-
-            GUILayout.BeginHorizontal();
-            GUILayout.Label("Mean thrust: ", GUILayout.ExpandWidth(true));
-            GUILayout.Label("" + save.GetActiveMeanThrust(), GUILayout.ExpandWidth(false));
-            GUILayout.EndHorizontal();
-            save.SetActiveMeanThrust(GUILayout.HorizontalSlider(save.GetActiveMeanThrust(), 80f, 120f));
-
-            detectStearingThrusters = GUILayout.Toggle(detectStearingThrusters, "Detect reaction control thrusters");
-            if (detectStearingThrusters)
-            {
-                GUILayout.Label("Stearing Threshold: " + (Math.Acos(minEfficiency) * 180 / Math.PI) + "°");
-                minEfficiency = Mathf.Cos(GUILayout.HorizontalSlider(Mathf.Acos(minEfficiency), 0f, Mathf.PI / 2));
-                GUILayout.BeginHorizontal();
-                GUILayout.Label("Direction");
-                InsertDropdownboxDirection();
-                GUILayout.EndHorizontal();
-                GUILayout.BeginHorizontal();
-                GUILayout.Label("");
-                if (direction == thrustDirections.custom)
-                {
-                    if (GUILayout.Button("Open/Close Direction Selector")) { showDirectionSelector = !showDirectionSelector; }
-                }
-                GUILayout.EndHorizontal();
-                //showDirection = GUILayout.Toggle(showDirection, "Show direction");        //Is kinda broken
-            }
-            //contUpdate = GUILayout.Toggle(contUpdate, "Continuous engine update");
-            //if (!contUpdate)
-            //{
-            //    if (GUILayout.Button("recalculate engine torque"))
-            //    {
-            //        enginesCounted = false;
-            //    }
-            //}
-
-            showEngines = GUILayout.Toggle(showEngines, "show/hide engine information");
-            if (showEngines)
-            {
-                GUILayout.BeginHorizontal();
-                GUILayout.Label("torque demand: ", GUILayout.ExpandWidth(true));
-                GUILayout.Label("" + demand.ToString(), GUILayout.ExpandWidth(false));
-                GUILayout.EndHorizontal();
-                GUILayout.BeginHorizontal();
-                GUILayout.Label("Thrust Axis: ", GUILayout.ExpandWidth(true));
-                GUILayout.Label("" + mainThrustAxis.ToString(), GUILayout.ExpandWidth(false));
-                GUILayout.EndHorizontal();
-                //GUILayout.BeginHorizontal();
-                //GUILayout.Label("ReferenceTransformPart: ", GUILayout.ExpandWidth(true));
-                //GUILayout.Label("" + vessel.GetReferenceTransformPart(), GUILayout.ExpandWidth(false));
-                //GUILayout.EndHorizontal();
-                //GUILayout.BeginHorizontal();
-                //GUILayout.Label("ReferenceTransformPartID: ", GUILayout.ExpandWidth(true));
-                //GUILayout.Label("" + vessel.referenceTransformId, GUILayout.ExpandWidth(false));
-                //GUILayout.EndHorizontal();
-                //GUILayout.BeginHorizontal();
-                //GUILayout.Label("ReferenceTransformRot: ", GUILayout.ExpandWidth(true));
-                //GUILayout.Label("" + (vessel.GetReferenceTransformPart().orgRot * Vector3.down).ToString(), GUILayout.ExpandWidth(false));
-                //GUILayout.EndHorizontal();
-                positionScrollViewEngines = GUILayout.BeginScrollView(positionScrollViewEngines, GUILayout.Height(300));
-                foreach (EngineWrapper eng in engineTable)
-                {
-                    GUILayout.Label(eng.getName() + " \r\n" +
-                                    "steering vector: " + eng.steeringVector + "\r\n" +
-                                    "thrustvector: " + eng.thrustVector + "\r\n" +
-                                    "Steering: " + eng.steering +
-                                    " Efficiancy: " + eng.efficiency + "\r\n" +
-                                    "Thrust: " + eng.thrustPercentage.ToString()
-                                    );
-                }
-                GUILayout.EndScrollView();
-            }
-            else { GUILayout.Label("."); }
-
-            GUILayout.EndVertical();
-            GUI.DragWindow();
-
-
-        }
-
-        private void WindowDirectionSelector(int windowID)
-        {
-            GUILayout.BeginVertical();
-            GUILayout.Label("pitch: " + Mathf.RoundToInt(customDirectionVector.x) + "°");
-            GUILayout.BeginHorizontal();
-            for (int a = -90; a < 200; a += 90) { if (GUILayout.Button(a.ToString() + "°")) { customDirectionVector.x = a; } }
-            GUILayout.EndHorizontal();
-            customDirectionVector.x = GUILayout.HorizontalSlider(customDirectionVector.x, -180f, 180f);
-            GUILayout.Label("yaw: " + Mathf.RoundToInt(customDirectionVector.y) + "°");
-            GUILayout.BeginHorizontal();
-            for (int a = -90; a < 200; a += 90) { if (GUILayout.Button(a.ToString() + "°")) { customDirectionVector.y = a; } }
-            GUILayout.EndHorizontal();
-            customDirectionVector.y = GUILayout.HorizontalSlider(customDirectionVector.y, -180f, 180f);
-            GUILayout.EndVertical();
-            GUI.DragWindow();
-        }
-		
-        /// <summary>
-        /// Makes sure the toolbar icon resembles the present situation
-        /// </summary>
-		private void UpdateToolbarIcon() {
-            if (isActive)
-            {
-                if (ElectricChargeAvailible(vessel))
-                {
-                    if (reverseThrust)
-                        TCAButton.TexturePath = "ThrottleControlledAvionics/textures/icon_button_R";
-                    else
-                        TCAButton.TexturePath = "ThrottleControlledAvionics/textures/icon_button_on";
-                }
-                else { TCAButton.TexturePath = "ThrottleControlledAvionics/textures/icon_button_noCharge"; }
-            }
-            else
-                TCAButton.TexturePath = "ThrottleControlledAvionics/textures/icon_button_off";
-		}
-		
 		/// <summary>
-		/// Inserts a dropdownbox to select the deired saved settings from
+		/// This funtion will draw the direction specifid by the chosen direction. Momentarly it will also draw th fwdVector of the craft.
 		/// </summary>
-		private void InsertDropdownboxSave()
+		public void DrawDirection()
 		{
-			int i = saveListBox.GetSelectedItemIndex();
-			i = saveListBox.List( saveList[i].text, saveList, "Box");
-			save.SetActiveSave(i);
+			//Debug.Log("trying to draw line from " + vessel.findWorldCenterOfMass() +", in direction: " + mainThrustAxis);
+			Vector3 direction = mainThrustAxis.normalized * 5;
+			Material whiteDiffuseMat = new Material(Shader.Find("Unlit/Texture"));
+			
+			GameObject DirectionLine = new GameObject("direction line");
+			LineRenderer directionLine = DirectionLine.AddComponent<LineRenderer>();
+			directionLine.SetWidth(-0.1f, 0.1f);
+			directionLine.SetPosition(0, vessel.findWorldCenterOfMass());
+			directionLine.SetPosition(1, vessel.findWorldCenterOfMass() + direction + vessel.GetFwdVector());
+			directionLine.SetColors(Color.red, Color.red);
+			directionLine.useWorldSpace = true;
+			directionLine.material = whiteDiffuseMat;
+			Destroy(DirectionLine.renderer.material,0.04f);
+			Destroy(DirectionLine,0.04f);
+			
+			
+			GameObject FwdLine = new GameObject("fwd line");
+			LineRenderer fwdLine = FwdLine.AddComponent<LineRenderer>();
+			fwdLine.SetWidth(-0.1f, 0.1f);
+			fwdLine.SetPosition(0, vessel.findWorldCenterOfMass());
+			fwdLine.SetPosition(1, vessel.findWorldCenterOfMass() + vessel.GetFwdVector()*1000);
+			fwdLine.SetColors(Color.green, Color.green);
+			fwdLine.useWorldSpace = true;
+			fwdLine.material = whiteDiffuseMat;
+			Destroy(FwdLine.renderer.material, 0.1f);
+			Destroy(FwdLine, 0.11f);
+			
 		}
 
-        /// <summary>
-        /// Inserts a dropdownbox to select the desired direction
-        /// </summary>
-        private void InsertDropdownboxDirection()
-        {
-            int i = directionListBox.GetSelectedItemIndex();
-            i = directionListBox.List(directionList[i].text, directionList, "Box");
-            switch(i)
-            {
-                case 0:
-                    direction = thrustDirections.up;
-                    break;
-                case 1:
-                    direction = thrustDirections.mean;
-                    break;
-                case 2:
-                    direction = thrustDirections.custom;
-                    
-                    break;
-                default:
-                    Debug.LogError("Invalid direction given");
-                    break;
-                
-            }
-        }
-		
-		private void windowHelp(int windowID)
-		{
-			String instructions = "Welcome to the instructions manual.\nFor simple use:\n\t 1)Put TCA on ('y'),\n\t 2)Put SAS on ('t'), \n\t 3) Launch \n\n"+
-				"For more advanced use:\n\t -The sensitivity determines the amount of thrust differences TCA will utilise. A high value will give a very fast and abrupt respose, a low value will be a lot smoother"+
-					", but might not be as fast.\n\t -Mean thrust is the virtual average thrust. A value below 100 means that the engines will be started throttled down and "+
-					"will correct by both throttling up and down. A value above 100 means that they will wait untill the deviation becomes rather big. This might be good if "+
-					"you think that the standard avionics are strong enough to handle the load. \n\t -3 different settings can be saved at the same time. Saving happens automaticly."+
-                    " \n\t -Detect reaction control thrusters will cause engines that are not sufficiently aligned with the direction you want to go in to only be used as reaction control engines. " +
-                    "This direction can be chosen as up, where normal rockets or planes want to go to; mean, which takes the weighted avarage of all the engines and tries to go that way, "+
-                    "or you can chose the direction yourself with custom. The stearing threshold is the maximum angle between an engine and the desired direction so that that engine will fire "+
-                    "(near) full throttle. A higher angle will result in more engines firing, but with potential less efficiency" +
-					"\n\nWarning: \n\t -TCA assumes that the engine bells are aligned allong the y-axis of the engine parts. This is the standard orientation for most of them. "+
-					"If you possess engines that can change the orientation of their engine bells, like some form 'Ferram aerospace' please make sure that they are alligned allong"+
-					" the right axis before enabeling TCA. \n\t -Note that while jet engines can be throttled, they have some latancy, what might controling VTOL's based on these"+
-					" rather tricky. \n\t SRB's can't be controlled. That's because they're SRB's";
-			GUILayout.Label(instructions, GUILayout.MaxWidth(400));
-			GUI.DragWindow();
-		}
-
-        /// <summary>
-        /// This funtion will draw the direction specifid by the chosen direction. Momentarly it will also draw th fwdVector of the craft.
-        /// </summary>
-        private void DrawDirection()
-        {
-            //Debug.Log("trying to draw line from " + vessel.findWorldCenterOfMass() +", in direction: " + mainThrustAxis);
-            Vector3 direction = mainThrustAxis.normalized * 5;
-            Material whiteDiffuseMat = new Material(Shader.Find("Unlit/Texture"));
-
-            GameObject DirectionLine = new GameObject("direction line");
-            LineRenderer directionLine = DirectionLine.AddComponent<LineRenderer>();
-            directionLine.SetWidth(-0.1f, 0.1f);
-            directionLine.SetPosition(0, vessel.findWorldCenterOfMass());
-            directionLine.SetPosition(1, vessel.findWorldCenterOfMass() + direction + vessel.GetFwdVector());
-            directionLine.SetColors(Color.red, Color.red);
-            directionLine.useWorldSpace = true;
-            directionLine.material = whiteDiffuseMat;
-            Destroy(DirectionLine.renderer.material,0.04f);
-            Destroy(DirectionLine,0.04f);
-
-
-            GameObject FwdLine = new GameObject("fwd line");
-            LineRenderer fwdLine = FwdLine.AddComponent<LineRenderer>();
-            fwdLine.SetWidth(-0.1f, 0.1f);
-            fwdLine.SetPosition(0, vessel.findWorldCenterOfMass());
-            fwdLine.SetPosition(1, vessel.findWorldCenterOfMass() + vessel.GetFwdVector()*1000);
-            fwdLine.SetColors(Color.green, Color.green);
-            fwdLine.useWorldSpace = true;
-            fwdLine.material = whiteDiffuseMat;
-            Destroy(FwdLine.renderer.material, 0.1f);
-            Destroy(FwdLine, 0.11f);
-            
-        }
 		#endregion
 		
 		
@@ -434,7 +190,7 @@ namespace ThrottleControlledAvionics
         /// </summary>
         /// <param name="v">The vessel</param>
         /// <returns>True if there is charge left</returns>
-        bool ElectricChargeAvailible(Vessel v)
+        public bool ElectricChargeAvailible(Vessel v)
         {
             List<Vessel.ActiveResource> ars = vessel.GetActiveResources();
             foreach (Vessel.ActiveResource ar in ars)
@@ -504,6 +260,7 @@ namespace ThrottleControlledAvionics
         /// <returns>The normalized avarage of all thrust, or preset directions</returns>
         private Vector3 DetermineMainThrustAxis(List<EngineWrapper> engineTable)
         {
+			Destroy (null);
             switch (direction)
             {
                 case thrustDirections.mean:
@@ -612,7 +369,7 @@ namespace ThrottleControlledAvionics
 		internal void OnDestroy()
 		{
 			save.Save();
-			TCAButton.Destroy();    // cleaning up
+			//TCAButton.Destroy();    // cleaning up
 		}
 		
 		
