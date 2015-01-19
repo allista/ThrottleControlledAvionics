27f82821b979fdc67d895c6f55be10e779323268
Allis Tauri <allista@gmail.com>
2015-01-19 15:06:50 +0300

    Corrected KSPAssembly version.

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
 [assembly: AssemblyVersion("2.0.0")]
-[assembly: KSPAssembly("ThrottleControlledAvionics", 0, 0)]
+[assembly: KSPAssembly("ThrottleControlledAvionics", 2, 0)]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
