d1168860464865e3b1484ecb42df1a779dd50b99
Allis Tauri <allista@gmail.com>
2015-01-21 18:13:45 +0300

    Fixed KSP .version generation. The file generated is now the valid strict JSON. Closed #1.
    Checked with http://jsonlint.com/

--- KSP-AVC-updater/Program.cs
+++ KSP-AVC-updater/Program.cs
@@ -17,23 +17,23 @@ namespace KSPAVCupdater
     ""CHANGE_LOG_URL"":""{3}"",
     ""VERSION"":
      {{", KSP_AVC_Info.Name, KSP_AVC_Info.VersionURL, KSP_AVC_Info.UpgradeURL, KSP_AVC_Info.ChangeLogURL);
-			file.WriteLine("         \"MAJOR\":{0}", KSP_AVC_Info.HangarVersion.Major);
-			file.WriteLine("         \"MINOR\":{0}", KSP_AVC_Info.HangarVersion.Minor);
-			file.WriteLine("         \"PATCH\":{0}", KSP_AVC_Info.HangarVersion.Build);
+			file.WriteLine("         \"MAJOR\":{0},", KSP_AVC_Info.HangarVersion.Major);
+			file.WriteLine("         \"MINOR\":{0},", KSP_AVC_Info.HangarVersion.Minor);
+			file.WriteLine("         \"PATCH\":{0},", KSP_AVC_Info.HangarVersion.Build);
 			file.WriteLine("         \"BUILD\":{0}", KSP_AVC_Info.HangarVersion.Revision);
 			file.WriteLine(
-@"     }
+@"     },
     ""KSP_VERSION_MIN"":
      {");
-			file.WriteLine("         \"MAJOR\":{0}", KSP_AVC_Info.MinKSPVersion.Major);
-			file.WriteLine("         \"MINOR\":{0}", KSP_AVC_Info.MinKSPVersion.Minor);
+			file.WriteLine("         \"MAJOR\":{0},", KSP_AVC_Info.MinKSPVersion.Major);
+			file.WriteLine("         \"MINOR\":{0},", KSP_AVC_Info.MinKSPVersion.Minor);
 			file.WriteLine("         \"PATCH\":{0}", KSP_AVC_Info.MinKSPVersion.Build);
 			file.WriteLine(
-@"     }
+@"     },
     ""KSP_VERSION_MAX"":
      {");
-			file.WriteLine("         \"MAJOR\":{0}", KSP_AVC_Info.MaxKSPVersion.Major);
-			file.WriteLine("         \"MINOR\":{0}", KSP_AVC_Info.MaxKSPVersion.Minor);
+			file.WriteLine("         \"MAJOR\":{0},", KSP_AVC_Info.MaxKSPVersion.Major);
+			file.WriteLine("         \"MINOR\":{0},", KSP_AVC_Info.MaxKSPVersion.Minor);
 			file.WriteLine("         \"PATCH\":{0}", KSP_AVC_Info.MaxKSPVersion.Build);
 			file.WriteLine(
 @"     }
