dc12cdc24ec962575717797ff0aeabf0210888dc
Allis Tauri <allista@gmail.com>
2015-01-29 13:40:21 +0300

    Corrected a label.

--- TCAGui.cs
+++ TCAGui.cs
@@ -277,7 +277,7 @@ namespace ThrottleControlledAvionics
 			GUILayout.EndHorizontal();
 			GUILayout.BeginHorizontal();
 			TCA.CFG.BlockThrottle = GUILayout.Toggle(TCA.CFG.BlockThrottle, 
-			                                         "Control vertical speed with throttle controls", 
+			                                         "Set vertical speed with throttle controls.", 
 			                                         GUILayout.ExpandWidth(false));
 			TCA.CFG.VSControlSensitivity = Utils.FloatSlider("Sensitivity", TCA.CFG.VSControlSensitivity, 0.001f, 0.05f, "P2");
 			GUILayout.EndHorizontal();
