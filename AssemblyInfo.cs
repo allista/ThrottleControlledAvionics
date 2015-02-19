5fecd69f8b4f0dfd266cea19c8badf0e2697cef3
Allis Tauri <allista@gmail.com>
2015-02-19 23:06:46 +0300

    Changed version to 2.1.1

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyVersion("2.1.1")]
 [assembly: KSPAssembly("ThrottleControlledAvionics", 2, 1)]
 
 // The following attributes are used to specify the signing key for the assembly, 
