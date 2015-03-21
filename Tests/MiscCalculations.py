94a3ca14e40b4f2b222ba78d189724f2a2ccd103
Allis Tauri <allista@gmail.com>
2015-03-21 09:52:58 +0300

    Many changes to Attitude simulations; added linalg simulation.

--- Tests/MiscCalculations.py
+++ Tests/MiscCalculations.py
@@ -274,179 +274,6 @@ def sim_PID():
     pid2.sim(PV, SV, T)
     plt.show()
     
-    
-def sim_VSpeed():
-    sim1 = sim(10.0, 1, 10, 10.0)
-    sim2 = sim(10.0, 1, 20, 10.0)
-    
-    start_v = 50
-    
-    sim1.run(start_v)
-    sim1.plot()
-    sim2.run(start_v)
-    sim2.plot()
-    plt.show()
-    if start_v == 0: sys.exit()
-    sim1.run(-start_v)
-    sim1.plot()
-    sim2.run(-start_v)
-    sim2.plot()
-    plt.show()
-    
-def sim_Attitude():
-    def S(T, K, L=0): return vec.sum(Tk(T, K, L))
-    def Sm(T, K, L=0): return abs(S(T,K))
-    def Tk(T, K, L=0): return [t*clampL(k, L) for t, k in zip(T,K)]
-    
-    def opt(target, engines, eps):
-        tm = abs(target)
-        comp = vec()
-        for e in engines:
-            p = e.current_torque*target
-            e.limit_tmp = -p/tm/abs(e.current_torque) if p < 0 else 0.0
-            if e.limit_tmp > 0:
-                comp += e.nominal_current_torque( e.limit)
-        compm = abs(comp)
-        if compm < eps: return False
-        limits_norm = clamp01(tm/compm)
-        for e in engines:
-            e.limit *= 1.0 - clamp01(e.limit_tmp*limits_norm)
-        return True
-    
-    def optR(engines, D=vec(), vK=1.0, eps=0.1, maxI=500, output=True):
-        torque_clamp = vec6()
-        torque_imbalance = vec()
-        ti_min = vec(); ti_max = vec();
-        for e in engines:
-            e.limit = 1.0
-            ti_min += e.nominal_current_torque(0)
-            ti_max += e.nominal_current_torque(1)
-        if ti_min > 0 and ti_max > 0: 
-            vK = clampL(vK, clamp01(abs(ti_min)/abs(ti_max)))
-        for e in engines:
-            e.current_torque = e.nominal_current_torque(vK)
-            torque_imbalance += e.current_torque
-            torque_clamp.add(e.current_torque)
-        _d = torque_clamp.clamp(D)
-        if output:
-            print 'vK: %f' % vK
-            print 'Torque clamp:\n', torque_clamp
-            print 'demand:        ', D
-            print 'clamped demand:', _d
-            print 'min TI: %s, max TI %s' % (ti_min, ti_max)
-            print ('initial     %s, error %s, dir error %s' % 
-                   (torque_imbalance, abs(torque_imbalance-_d), torque_imbalance.angle(_d)))
-        s = []; s1 = []; i = 0;
-        for i in xrange(maxI):
-            s.append(abs(torque_imbalance-_d))
-            s1.append(torque_imbalance.angle(_d) if abs(_d) > 0 else 0)
-#             if len(s1) > 1 and s1[-1] < 55 and s1[-1]-s1[-2] > eps: break
-            if s[-1] < eps or len(s) > 1 and abs(s[-1]-s[-2]) < eps: break
-            mlim = max(e.limit for e in engines)
-            for e in engines: e.limit = clamp01(e.limit/mlim)
-            if not opt(D-torque_imbalance, engines, eps): break
-            torque_imbalance = vec.sum(e.nominal_current_torque(vK * e.limit) for e in engines)
-        if output:
-            ##########
-            print 'iterations:', i
-            print 'limits:    ', list(e.limit for e in engines)
-            print 'result      %s, error %s, dir error %s' % (torque_imbalance, s[-1], s1[-1])
-            print 'engines:   ', list(e.nominal_current_torque(vK * e.limit) for e in engines)
-            print
-            ##########
-            x = np.arange(len(s))
-            plt.subplot(2,1,1)
-            plt.plot(x, s, '-')
-            plt.xlabel('iterations')
-            plt.ylabel('torque error (kNm)')
-            plt.subplot(2,1,2)
-            plt.plot(x, s1, '-')
-            plt.xlabel('iterations')
-            plt.ylabel('torque direction error (deg)')
-            ##########
-        return s[-1], s1[-1]
-    
-    VTOL_Test = [
-                    engine(vec(-3.4, -2.0, 0.0), min_thrust=0.0, max_thrust=250.0),
-                    engine(vec(-3.4, 2.0, 0.0),  min_thrust=0.0, max_thrust=250.0),
-                    engine(vec(3.5, -2.0, 0.0),  min_thrust=0.0, max_thrust=250.0),
-                    engine(vec(1.5, 2.0, 0.0),   min_thrust=0.0, max_thrust=250.0),
-                    engine(vec(1.3, 2.0, 1.6),   min_thrust=0.0,  max_thrust=20.0),
-                    engine(vec(3.1, -2.0, -3.7), min_thrust=0.0,  max_thrust=20.0),
-                    engine(vec(-3.1, -2.0, 3.7), min_thrust=0.0,  max_thrust=20.0),
-                    engine(vec(-2.4, 2.0, -2.9), min_thrust=0.0,  max_thrust=20.0),
-                 ]
-    
-    VTOL_Test_Bad_Demand = [
-                            vec(0.0, 0.0, 0.0),
-                            vec(100.0, 0.0, 0.0),
-                            vec(0.0, 100.0, 0.0),
-                            vec(0.0, 0.0, 100.0),
-                                
-                            vec(-1797.147, 112.3649, 80.1167), #Torque Error: 165.3752
-                            vec(1327.126, -59.91731, 149.1847), #Torque Error: 229.8387
-                            vec(107.5895, -529.4326, -131.0672), #Torque Error: 59.95838
-                            vec(50.84914, -1.706408, 113.4622), #Torque Error: 25.10385
-                            vec(-0.4953138, 0.2008617, 39.52808),
-                            vec(14.88248, 0.9660782, -51.20171),
-                            vec(-20.34281, -10.67025, 38.88113),
-                            ]
-    
-    Hover_Test = [
-                    engine(vec(-6.2, 6.4, 0.6), max_thrust=450),
-                    engine(vec(-6.2, -6.4, -0.6), max_thrust=450),
-                    engine(vec(3.9, 7.4, 0.6), max_thrust=450),
-                    engine(vec(3.9, -6.4, -0.6), max_thrust=450)
-                ]
-    
-    Hover_Test_Bad_Demand = [
-                             vec(0.2597602, -5.2778279, 0.8444),
-                             vec(0.9042788, -1.347212, 483.4898), #Torque Error: 483.4926kNm, 90deg
-                             vec(-0.7012861, 0.2607862, -294.2217), #Torque Error: 339.2753kNm, 95.00191deg
-                             vec(0.2597602, -0.2778279, 295.8444), #Torque Error: 341.1038kNm, 94.96284deg
-                             ]
-    
-    for d in VTOL_Test_Bad_Demand: 
-        optR(VTOL_Test, d, vK=0.37, eps=0.1, maxI=30)
-    plt.show()
-    print '='*80+'\n\n'
-    
-    for d in Hover_Test_Bad_Demand+VTOL_Test_Bad_Demand: 
-        optR(Hover_Test, d, vK=0.37, eps=0.1, maxI=30)
-    plt.show()
-    print '='*80+'\n\n'
-
-#     N = range(500)
-#     np.random.seed(42)
-#     random_tests = [vec(200*np.random.random()-100.0,
-#                         200*np.random.random()-100.0,
-#                         200*np.random.random()-100.0) 
-#                     for _n in N]
-#     E = []; A = []; X = []; Y = []; Z = [];
-#     for d in random_tests:
-#         e, a = optR(Hover_Test, d, vK=0.5, eps=0.1, maxI=30, output=False)
-#         dm = abs(d)
-#         E.append(e/dm*100); A.append(a);
-#         X.append(d[0]/dm*100); Y.append(d[1]/dm*100); Z.append(d[2]/dm*100)
-#         
-#     plt.subplot(4,1,1)
-#     plt.plot(A, E, 'o')
-#     plt.xlabel('angle')
-#     plt.ylabel('torque error (%)')
-#     plt.subplot(4,1,2)
-#     plt.plot(X, A, 'o')
-#     plt.xlabel('x %')
-#     plt.ylabel('angle')
-#     plt.subplot(4,1,3)
-#     plt.plot(Y, A, 'o')
-#     plt.xlabel('y %')
-#     plt.ylabel('angle')
-#     plt.subplot(4,1,4)
-#     plt.plot(Z, A, 'o')
-#     plt.xlabel('z %')
-#     plt.ylabel('angle')
-#     plt.show()
-    
 
 def sim_PIDf():
     MoI = vec(3.517439, 5.191057, 3.517369)
@@ -483,6 +310,204 @@ def sim_PIDf():
 #     [
 #      
 #      ]
+
+def sim_VSpeed():
+    sim1 = sim(10.0, 1, 10, 10.0)
+    sim2 = sim(10.0, 1, 20, 10.0)
+    
+    start_v = 50
+    
+    sim1.run(start_v)
+    sim1.plot()
+    sim2.run(start_v)
+    sim2.plot()
+    plt.show()
+    if start_v == 0: sys.exit()
+    sim1.run(-start_v)
+    sim1.plot()
+    sim2.run(-start_v)
+    sim2.plot()
+    plt.show()
+    
+VTOL_Test = [
+                engine(vec(-3.4, -2.0, 0.0), min_thrust=0.0, max_thrust=250.0),
+                engine(vec(-3.4, 2.0, 0.0),  min_thrust=0.0, max_thrust=250.0),
+                engine(vec(3.5, -2.0, 0.0),  min_thrust=0.0, max_thrust=250.0),
+                engine(vec(1.5, 2.0, 0.0),   min_thrust=0.0, max_thrust=250.0),
+                engine(vec(1.3, 2.0, 1.6),   min_thrust=0.0,  max_thrust=20.0),
+                engine(vec(3.1, -2.0, -3.7), min_thrust=0.0,  max_thrust=20.0),
+                engine(vec(-3.1, -2.0, 3.7), min_thrust=0.0,  max_thrust=20.0),
+                engine(vec(-2.4, 2.0, -2.9), min_thrust=0.0,  max_thrust=20.0),
+             ]
+
+Hover_Test = [
+                engine(vec(-6.2, 6.4, 0.6), max_thrust=450),
+                engine(vec(-6.2, -6.4, -0.6), max_thrust=450),
+                engine(vec(3.9, 7.4, 0.6), max_thrust=450),
+                engine(vec(3.9, -6.4, -0.6), max_thrust=450)
+            ]
+
+VTOL_Test_Bad_Demand = [
+                        vec(0.0, 0.0, 0.0),
+                        vec(10.0, 0.0, 0.0),
+                        vec(0.0, 10.0, 0.0),
+                        vec(0.0, 0.0, 10.0),
+                            
+                        vec(-1797.147, 112.3649, 80.1167), #Torque Error: 165.3752
+                        vec(1327.126, -59.91731, 149.1847), #Torque Error: 229.8387
+                        vec(107.5895, -529.4326, -131.0672), #Torque Error: 59.95838
+                        vec(50.84914, -1.706408, 113.4622), #Torque Error: 25.10385
+                        vec(-0.4953138, 0.2008617, 39.52808),
+                        vec(14.88248, 0.9660782, -51.20171),
+                        vec(-20.34281, -10.67025, 38.88113),
+                        ]
+
+Hover_Test_Bad_Demand = [
+                         vec(0.2597602, -5.2778279, 0.8444),
+                         vec(0.9042788, -1.347212, 483.4898), #Torque Error: 483.4926kNm, 90deg
+                         vec(-0.7012861, 0.2607862, -294.2217), #Torque Error: 339.2753kNm, 95.00191deg
+                         vec(0.2597602, -0.2778279, 295.8444), #Torque Error: 341.1038kNm, 94.96284deg
+                         ]
+    
+def sim_Attitude():
+    def S(T, K, L=0): return vec.sum(Tk(T, K, L))
+    def Sm(T, K, L=0): return abs(S(T,K))
+    def Tk(T, K, L=0): return [t*clampL(k, L) for t, k in zip(T,K)]
+    
+    def opt(target, angle, engines, eps):
+        tm = abs(target)
+        comp = vec()
+        for e in engines:
+            p = e.current_torque*target
+            e.limit_tmp = -p/tm/abs(e.current_torque) if p < 0 else 0.0
+            if e.limit_tmp > 0:
+                comp += e.nominal_current_torque(e.limit)
+        compm = abs(comp)
+        if compm < eps: return False
+        limits_norm = clamp01(tm/compm * angle/180 if angle > 0 else 1)
+        for e in engines:
+            e.limit *= 1.0 - clamp01(e.limit_tmp*limits_norm)
+        return True
+    
+    def optR(engines, D=vec(), vK=1.0, eps=0.1, maxI=500, output=True):
+        torque_clamp = vec6()
+        torque_imbalance = vec()
+        ti_min = vec(); ti_max = vec();
+        for e in engines:
+            e.limit = 1.0
+            ti_min += e.nominal_current_torque(0)
+            ti_max += e.nominal_current_torque(1)
+        if ti_min > 0 and ti_max > 0: 
+            vK = clampL(vK, clamp01(abs(ti_min)/abs(ti_max)))
+        for e in engines:
+            e.current_torque = e.nominal_current_torque(vK)
+            torque_imbalance += e.current_torque
+            torque_clamp.add(e.current_torque)
+        _d = torque_clamp.clamp(D)
+        if output:
+            print 'vK: %f' % vK
+            print 'Torque clamp:\n', torque_clamp
+            print 'demand:        ', D
+            print 'clamped demand:', _d
+            print 'min TI: %s, max TI %s' % (ti_min, ti_max)
+            print ('initial     %s, error %s, dir error %s' % 
+                   (torque_imbalance, abs(torque_imbalance-_d), torque_imbalance.angle(_d)))
+        s = []; s1 = []; i = 0;
+        for i in xrange(maxI):
+            s.append(abs(torque_imbalance-_d))
+            s1.append(torque_imbalance.angle(_d) if abs(_d) > 0 else 0)
+#             if len(s1) > 1 and s1[-1] < 55 and s1[-1]-s1[-2] > eps: break
+#             if s1[-1] > 0:
+#                 if s1[-1] < eps or len(s1) > 1 and abs(s1[-1]-s1[-2]) < eps*eps: break
+#             elif 
+            if s[-1] < eps or len(s) > 1 and abs(s[-1]-s[-2]) < eps/10.0: break
+            mlim = max(e.limit for e in engines)
+            for e in engines: e.limit = clamp01(e.limit/mlim)
+            if not opt(_d-torque_imbalance, s1[-1], engines, eps): break
+            torque_imbalance = vec.sum(e.nominal_current_torque(vK * e.limit) for e in engines)
+        mlim = max(e.limit for e in engines)
+        for e in engines: e.limit = clamp01(e.limit/mlim)
+        if output:
+            min_i = s1.index(min(s1)) if s1[-1] > 0 else s.index(min(s))  
+            ##########
+            print 'iterations:', i+1
+#             print 'dAngle:    ', abs(s1[-1]-s1[-2])
+            print 'limits:    ', list(e.limit for e in engines)
+            print 'result      %s, error %s, dir error %s' % (torque_imbalance, s[min_i], s1[min_i])
+            print 'engines:   ', list(e.nominal_current_torque(vK * e.limit) for e in engines)
+            print
+            ##########
+            x = np.arange(len(s))
+            plt.subplot(2,1,1)
+            plt.plot(x, s, '-')
+            plt.xlabel('iterations')
+            plt.ylabel('torque error (kNm)')
+            plt.subplot(2,1,2)
+            plt.plot(x, s1, '-')
+            plt.xlabel('iterations')
+            plt.ylabel('torque direction error (deg)')
+            ##########
+        return s[-1], s1[-1]
+    
+    for d in VTOL_Test_Bad_Demand: 
+        optR(VTOL_Test, d, vK=0.1, eps=0.1, maxI=200)
+    plt.show()
+    print '='*80+'\n\n'
+    
+#     for d in Hover_Test_Bad_Demand+VTOL_Test_Bad_Demand: 
+#         optR(Hover_Test, d, vK=0.2, eps=0.1, maxI=30)
+#     plt.show()
+#     print '='*80+'\n\n'
+
+#     N = range(500)
+#     np.random.seed(42)
+#     random_tests = [vec(200*np.random.random()-100.0,
+#                         200*np.random.random()-100.0,
+#                         200*np.random.random()-100.0) 
+#                     for _n in N]
+#     E = []; A = []; X = []; Y = []; Z = [];
+#     for d in random_tests:
+#         e, a = optR(Hover_Test, d, vK=0.5, eps=0.1, maxI=30, output=False)
+#         dm = abs(d)
+#         E.append(e/dm*100); A.append(a);
+#         X.append(d[0]/dm*100); Y.append(d[1]/dm*100); Z.append(d[2]/dm*100)
+#         
+#     plt.subplot(4,1,1)
+#     plt.plot(A, E, 'o')
+#     plt.xlabel('angle')
+#     plt.ylabel('torque error (%)')
+#     plt.subplot(4,1,2)
+#     plt.plot(X, A, 'o')
+#     plt.xlabel('x %')
+#     plt.ylabel('angle')
+#     plt.subplot(4,1,3)
+#     plt.plot(Y, A, 'o')
+#     plt.xlabel('y %')
+#     plt.ylabel('angle')
+#     plt.subplot(4,1,4)
+#     plt.plot(Z, A, 'o')
+#     plt.xlabel('z %')
+#     plt.ylabel('angle')
+#     plt.show()
+
+
+def linalg_Attitude():
+    
+    def engines2a(engines):
+        num_e = len(engines)
+        a = np.array([[None]*num_e, [None]*num_e, [None]*num_e], dtype=float)
+        for i, e in enumerate(engines):
+            t = e.nominal_current_torque(1)
+            a[0,i] = t[0]
+            a[1,i] = t[1]
+            a[2,i] = t[2]
+        return a
+    
+    for d in VTOL_Test_Bad_Demand: 
+        a = engines2a(VTOL_Test)
+        x, r, R, s = np.linalg.lstsq(a, d.v)
+        print "demand: %s\nlimits: %s\nresids: %s" % (d, x, np.matrix(a).dot(x)-d.v)
+        print ''
     
 #==================================================================#
     
