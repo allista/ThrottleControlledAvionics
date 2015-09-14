cfc6184755d9a34147d402f1c52a1bed7805de07
Allis Tauri <allista@gmail.com>
2015-09-14 18:29:50 +0300

    Trying to fix profile save-load issues on save-launch-revert usecases. WIP.

--- GUI/EnginesProfileEditor.cs
+++ GUI/EnginesProfileEditor.cs
@@ -19,8 +19,11 @@ namespace ThrottleControlledAvionics
 	public class EnginesProfileEditor : AddonWindowBase<EnginesProfileEditor>
 	{
 		const string LockName = "EnginesProfileEditor";
+		const string DefaultConstractName = "Untitled Space Craft";
+
 		NamedConfig CFG;
-		List<EngineWrapper> Engines = new List<EngineWrapper>();
+		bool saved;
+		readonly List<EngineWrapper> Engines = new List<EngineWrapper>();
 
 		public static bool GUIVisible 
 		{ 
@@ -34,41 +37,46 @@ namespace ThrottleControlledAvionics
 			height = 400;
 			GameEvents.onEditorShipModified.Add(OnShipModified);
 			GameEvents.onEditorLoad.Add(OnShipLoad);
-			GameEvents.onEditorRestart.Add(OnEditorRestart);
+			GameEvents.onEditorRestart.Add(Reset);
 		}
 
 		public override void OnDestroy ()
 		{
+			if(EditorLogic.fetch.ship.shipName != DefaultConstractName) SaveConfig();
 			GameEvents.onEditorShipModified.Remove(OnShipModified);
 			GameEvents.onEditorLoad.Remove(OnShipLoad);
-			GameEvents.onEditorRestart.Remove(OnEditorRestart);
+			GameEvents.onEditorRestart.Remove(Reset);
 			base.OnDestroy();
 		}
 
-		void OnEditorRestart()
+		void Reset()
 		{
+			TCAToolbarManager.ShowButton(false);
 			Engines.Clear();
+			saved = false;
 			CFG = null;
 		}
 
 		void OnShipLoad(ShipConstruct ship, CraftBrowser.LoadType load_type)
 		{
 			if(load_type == CraftBrowser.LoadType.Merge) return;
+			Utils.Log("EPE.OnShipLoad: {0}", ship.shipName);//debug
 			if(UpdateEngines(ship)) GetCFG(ship);
 		}
 
-		void GetCFG(ShipConstruct ship)
+		void GetCFG(ShipConstruct ship, bool local = false)
 		{
 			CFG = TCAScenario.GetConfig(ship.shipName);
-			if(CFG != null) 
-				CFG.EnginesProfiles.Active.Apply(Engines);
-			else 
-			{ 
-				CFG = TCAScenario.NewNamedConfig(ship.shipName);
+			if(CFG == null)
+			{
+				saved = !local;
+				CFG = local ? new NamedConfig(ship.shipName) : 
+					TCAScenario.NewNamedConfig(ship.shipName);
 				CFG.EnginesProfiles.AddProfile(Engines);
 			}
-			Utils.Log("EPE.UpdateCFG.CFG: {0}", CFG);//debug
-			CFG.EnginesProfiles.Active.Update(Engines);
+			else { CFG.ActiveProfile.Apply(Engines); saved = true; }
+			CFG.ActiveProfile.Update(Engines);
+			Utils.Log("EPE.GetCFG.CFG: {0}, local: {1}", CFG, local);//debug
 		}
 
 		bool UpdateEngines(ShipConstruct ship)
@@ -76,6 +84,7 @@ namespace ThrottleControlledAvionics
 			Engines.Clear();
 			if(ModuleTCA.HasTCA) 
 			{ 
+				TCAToolbarManager.SetDefaultButton();
 				TCAToolbarManager.ShowButton();
 				foreach(Part p in ship.Parts)
 					foreach(var module in p.Modules)
@@ -85,28 +94,47 @@ namespace ThrottleControlledAvionics
 					}
 				if(Engines.Count > 0) return true;
 			}
-			TCAToolbarManager.ShowButton(false);
-			CFG = null;
+			Reset();
 			return false;
 		}
 
 		void OnShipModified(ShipConstruct ship)
 		{
-			if(CFG == null) return;
 			if(!UpdateEngines(ship)) return;
-			CFG.EnginesProfiles.Active.Update(Engines);
-			if(ship.shipName != CFG.Name)
+			Utils.Log("EPE.OnShipModified: {0}", ship.shipName);//debug
+			if(CFG == null) GetCFG(ship, true);
+			CFG.ActiveProfile.Update(Engines);
+		}
+
+		void SaveConfig()
+		{
+			if(saved || CFG == null) return;
+			TCAScenario.NamedConfigs.Remove(CFG.Name);
+			CFG.Name = EditorLogic.fetch.ship.shipName;
+			TCAScenario.NamedConfigs.Add(CFG.Name, CFG);
+			saved = true;
+		}
+
+		public void Update()
+		{
+			if(CFG == null) return;
+			if(EditorLogic.fetch.ship.shipName != CFG.Name)
 			{
-				Utils.Log("EPE.OnShipModified.Rename {0} to {1}", CFG.Name, ship.shipName);//debug
-				TCAScenario.NamedConfigs.Remove(CFG.Name);
-				CFG.Name = ship.shipName;
-				TCAScenario.NamedConfigs.Add(CFG.Name, CFG);
+				Utils.Log("EPE.Udpate.Rename '{0}' to '{1}'", CFG.Name, EditorLogic.fetch.ship.shipName);//debug
+				SaveConfig();
 			}
 		}
 
 		protected override void DrawMainWindow(int windowID)
 		{
+			GUILayout.BeginVertical();
+			GUILayout.Label(new GUIContent(string.Format("{0} configuration: {1}", CFG.Name, saved? "saved" : "unsaved"),
+			                               "Configuration for 'Untitled Space Craft' is never saved"),
+			                saved? Styles.green : Styles.red, GUILayout.ExpandWidth(true));
 			CFG.EnginesProfiles.Draw(height);
+			GUILayout.EndVertical();
+			if(CFG.ActiveProfile.Changed)
+				CFG.ActiveProfile.Apply(Engines);
 			base.DrawMainWindow(windowID);
 		}
 
