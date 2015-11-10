0401c22cd08b6bd636fb474a14d1e4033ca10925
Allis Tauri <allista@gmail.com>
2015-11-10 08:37:53 +0300

    Making Releases directory befor zipping.

--- make-release.sh
+++ make-release.sh
@@ -8,6 +8,9 @@ assembly_info=AssemblyInfo.cs
 version=$(grep AssemblyVersion $assembly_info | sed "s:.*\"\(.*\)\".*:\1:")
 archive="$modname-v$version.zip"
 
+#create Releases dir
+mkdir -p Releases
+
 #create zip archive
 zip -r -9 Releases/$archive GameData -x "*~" "*/config.xml" "*/TCA.conf" || exit 1
 zip -T Releases/$archive || exit 2
