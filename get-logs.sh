9ec69a2fdf3df912b55997910592b7ab669199a7
Allis Tauri <allista@gmail.com>
2015-12-04 12:57:35 +0300

    Added make_release python script and using it to generate release and changelog in make-release.sh

--- get-logs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-[[ $# -eq 1 ]] || { echo "Provide a revision sha or other revision id (see 'man gitrevisions' for details)"; exit 1; }
-
-git --no-pager log "$1"..HEAD | grep -v -E "commit|Author|Date|^$" | sed -r -e 's/^ +/* /g'
--- make-release.sh
+++ make-release.sh
@@ -2,15 +2,4 @@
 
 cd $(dirname "$0")
 
-modname=ThrottleControlledAvionics
-assembly_info=AssemblyInfo.cs
-
-version=$(grep AssemblyVersion $assembly_info | sed "s:.*\"\(.*\)\".*:\1:")
-archive="$modname-v$version.zip"
-
-#create zip archive
-zip -r -9 Releases/$archive GameData -x "*~" "*/config.xml" "*/TCA.conf" || exit 1
-zip -T Releases/$archive || exit 2
-echo
-echo "$archive created in Releases/"
-
+./make_release -e '*/config.xml' '*/TCA.conf' '*/TCA.macro'
