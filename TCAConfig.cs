a3cb7013b613894ecd9d8436b245513cc56caa0f
Allis Tauri <allista@gmail.com>
2015-12-04 14:21:14 +0300

    Moved instructions to a separate text file. Should have it long ago.

--- TCAConfig.cs
+++ TCAConfig.cs
@@ -8,6 +8,7 @@
 // or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Collections.Generic;
@@ -20,6 +21,7 @@ namespace ThrottleControlledAvionics
 		new public const string NODE_NAME = "TCAGLOBALS";
 
 		public const string TCA_PART = "ThrottleControlledAvionics";
+		public const string INSTRUCTIONS = "INSTRUCTIONS.txt";
 
 		[Persistent] public bool  IntegrateIntoCareer  = true;
 		[Persistent] public bool  RoleSymmetryInFlight = true;
@@ -51,56 +53,16 @@ namespace ThrottleControlledAvionics
 		[Persistent] public TimeWarpControl.Config           WRP = new TimeWarpControl.Config();
 		[Persistent] public MatchVelocityAutopilot.Config    MVA = new MatchVelocityAutopilot.Config();
 
-		//help text
 		public string Instructions = string.Empty;
-		const string instructions = 
-@"Welcome to TCA instructions manual.
-
-For simple use:
-    1) Turn TCA on ('{0}')
-    2) Turn SAS on ('t')
-    3) Activate the engines and throttle them up!
-
-Engine Modes:
-    * In editor or in flight (through the part menu) you may set any engine to work in one of the three modes: 
-        1) Main Engine (default). TCA tries to maximize the thrust of these engines. In a perfectly balanced ship all Main Engines should have 100% thrust in the absence of control input. These engines are also used to control vertical speed.
-        2) Maneuver Engine. TCA tries to minimize the thrust of these engines. In a perfectly balanced ship these engines produce thrust only in response to control input
-        3) Manual Control. TCA does not change the thrust of these engines, but includes them in calculations.
-
-Autotuning Parameters:
-    * If this option is enabled, TCA calculates Steering Gains and PI coefficients as functions of maximum possible angular acceleration along each of the principal axes of a craft. Note, that calculations are based on the predefined response curves tuned for the stock SAS.
-    * If you have already tuned these parameters for the ship, save its configuration before enabling, as autotuning will overwrite previous parameters.
-
-Steering Gains:
-    * Master Gain modifies the magnitude of all steering input after other modifications. If the craft reacts slowly, increase it a little. If the craft vibrates after a maneuver, decrease it.
-    * Pitch, Yaw and Roll modify only corresponding inputs.
-    * Linking Pitch and Yaw useful if the ship's engines are symmetrical along main axis. In that case Pitch&Yaw slider may be used in stead of Master Gain.
-
-Engines PI-controller tuning:
-    * P (proportional) parameter controls the response speed of the engines. If the craft reacts slowly, increase it a little. If the craft vibrates after a maneuver, decrease it.
-    * I (integral) parameter controls the smoothing. It prevents rapid changes in thrust. If engines's trust jitters, increase it.
-
-Vertical Speed Control, hovering and horizontal flight:
-    * If you're using TCA to control VTOL or during vertical landing of a rocket, you may enable the Vertical Speed Control System.
-    * The desired vertical speed may be set with the scroll bar in the (configurable) interval from -{1:F1}m/s to {1:F1}m/s (not including). Then the total thrust of all controllable engines is modified in an attempt to reach the specified vertical speed. This speed, however, is never achieved, but approached asymptotically, so you need to set it a little higher (0.1-0.5m/s) than desired.
-    * To completely disable the VSC, just set it the desired speed to its maximum value ({1}m/s).
-    * VSC is also very useful to maintain stable horizontal flight. Consider a VTOL that has lifted off, reached some altitude and started to pitch to get some forward momentum. If the thrust of its engines will remain constant, it will start to loose altitude as it moves forward. But with the VSC the thrust will be adjusted, and the VTOL will move more or less in a plane.
-
-Set vertical speed with throttle controls:
-    * When this option is enabled, the throttle is locked at 100% and throttle controls are used to set desired vertical speed instead. If VSC system was switched off it is automatically enabled and the desired speed is set to 0.
-
-Kill Horizontal Velocity:
-    * Enables an autopilot that tries to maneuver the craft so that the horizontal component of its velocity became zero. It includes flip-over prevention system, so whatever the speed, the autopilot decrease it carefully and steady.
-
-Notes:
-    * For safety reasons the Vertical and Horizontal speed controls are disabled in orbit, but not on suborbital trajectories, so be carefull.
-    * If your ship wobbles and oscillates with TCA and SAS enabled, rebuild it with more struts, or decrease appropriate Steering Gains.
-    * Thrust of jets and turbofan engines changes very slowly. Thus using them as attitude controllers is impractical. If you want to use them, switch them to Manual Control mode.
-    * Solid boosters have constant thrust and thus cannot be controlled by TCA. But they are still accounted for, if present.";
 
 		public void Init()
 		{ 
-			Instructions = string.Format(instructions, ThrottleControlledAvionics.TCA_Key, VSC.MaxSpeed);
+			try
+			{
+				using(var file = new StreamReader(TCAScenario.PluginFolder(INSTRUCTIONS)))
+					Instructions = file.ReadToEnd();
+			}
+			catch { Instructions = string.Format("{0} file cannot be read", TCAScenario.PluginFolder(INSTRUCTIONS)); }
 			InputDeadZone *= InputDeadZone; //it is compared with the sqrMagnitude
 			//init all module configs
 			var mt = typeof(TCAModule.ModuleConfig);
