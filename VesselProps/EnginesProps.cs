d5caf92a76065b09af1d9f072689c0a54494a7fc
Allis Tauri <allista@gmail.com>
2016-02-21 08:46:19 +0300

    Fixed problem with airbreathing engines being stuck in flameout state after game reloading  when landed.

--- VesselProps/EnginesProps.cs
+++ VesselProps/EnginesProps.cs
@@ -68,8 +68,16 @@ namespace ThrottleControlledAvionics
 				VSL.ForceUpdateParts = false;
 			}
 			//unflameout engines
-			for(int i = 0; i < num_engines; i++)
-			{ var e = All[i]; if(e.engine.flameout) e.forceThrustPercentage(1); }
+			if(VSL.vessel.ctrlState.mainThrottle > 0)
+			{
+				for(int i = 0; i < num_engines; i++)
+				{ 
+					var e = All[i]; 
+					if(e.engine.flameout &&
+					   e.Role != TCARole.MANUAL) 
+						e.forceThrustPercentage(10); 
+				}
+			}
 			//sync with active profile
 			if(CFG.ActiveProfile.Activated) CFG.ActiveProfile.OnActivated(VSL);
 			if(ProfileSyncAllowed)
