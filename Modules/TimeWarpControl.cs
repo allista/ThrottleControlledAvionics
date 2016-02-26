d89544aaefed4bd0acd28a389bf83ee4e7a7f556
Allis Tauri <allista@gmail.com>
2016-02-26 08:32:53 +0300

    Improved DewarpTime calculation. Added StopWarp method and OnSOIChange callback.

--- Modules/TimeWarpControl.cs
+++ Modules/TimeWarpControl.cs
@@ -27,10 +27,32 @@ namespace ThrottleControlledAvionics
 
 		public double WarpToTime = -1;
 
+		public void StopWarp() { WarpToTime = 0; }
+
+		public override void Init()
+		{
+			base.Init();
+			GameEvents.onVesselSOIChanged.Add(OnSOIChanged);
+		}
+
+		void OnSOIChanged(GameEvents.HostedFromToAction<Vessel, CelestialBody> action)
+		{
+			if(TCA == null || action.host != VSL.vessel) return;
+			CFG.WarpToNode = false;
+			StopWarp();
+		}
+
 		protected override void Update()
 		{
-			if(WarpToTime <= VSL.Physics.UT) return;
-			var DewarpTime = WarpToTime-(WRP.DewarpTime+(1+TimeWarp.deltaTime)*(TimeWarp.CurrentRateIndex+1))-VSL.Physics.UT;
+			if(WarpToTime < 0) return;
+			if(WarpToTime <= VSL.Physics.UT && 
+			   TimeWarp.CurrentRateIndex == 0) 
+			{ WarpToTime = -1; return; }
+			//TimeWarp changes timescale from one rate the next in a second of a real time using Lerp: F+(T-F)*time
+			//so the time that will pass from rate F to rate T is: (F-T)/2
+			//and from rate F to rate 1: (F-1)/2
+			//but due to deltaTime steps it is safer to offset the dewarp time with just F+1
+			var DewarpTime = WarpToTime-(WRP.DewarpTime+TimeWarp.fetch.warpRates[TimeWarp.CurrentRateIndex]+1)-VSL.Physics.UT;
 			if(TimeWarp.CurrentRateIndex > 0 && DewarpTime < 0)
 				TimeWarp.SetRate(TimeWarp.CurrentRateIndex-1, false);
 			else if(DewarpTime > TimeWarp.CurrentRate && 
