3fb13205de9e06e16e85e744648ddf65f8239923
Allis Tauri <allista@gmail.com>
2016-02-28 19:04:21 +0300

    Working 1D optimization for a target that lies strictly in orbit plane.

--- Modules/DeorbitAutopilot.cs
+++ Modules/DeorbitAutopilot.cs
@@ -18,13 +18,17 @@ namespace ThrottleControlledAvionics
 	{
 		public class Config : TCAModule.ModuleConfig
 		{
-			[Persistent] public float StartOffset = 60f;  //sec
-			[Persistent] public float PeR         = 0.9f; //of planet radius
-			[Persistent] public float dVtol       = 0.01f; //of planet radius
+			[Persistent] public float StartOffset = 60f;   //s
+			[Persistent] public float PeR         = 0.9f;  //of planet radius
+			[Persistent] public float dVtol       = 0.01f; //m/s
+			[Persistent] public float Dtol        = 100f;  //m
 		}
 		static Config DEO { get { return TCAScenario.Globals.DEO; } }
 
 		public DeorbitAutopilot(ModuleTCA tca) : base(tca) {}
+
+		Orbit O { get { return VSL.vessel.orbitDriver.orbit; } }
+		CelestialBody Body { get { return VSL.vessel.orbitDriver.orbit.referenceBody; } }
 
 		ManeuverAutopilot MAN;
 
@@ -32,16 +36,16 @@ namespace ThrottleControlledAvionics
 //		double dVr = 0f;
 //		double dVn = 0f;
 
-		OrbitDBG new_orbit(Orbit old, Vector3d dV, double UT)
+		static Orbit new_orbit(Orbit old, Vector3d dV, double UT)
 		{
-			var obt = new OrbitDBG();
+			var obt = new Orbit();
 			var pos = old.getRelativePositionAtUT(UT);
 			var vel = old.getOrbitalVelocityAtUT(UT)+dV;
 			obt.UpdateFromStateVectors(pos, vel, old.referenceBody, UT);
 			return obt;
 		}
 
-		double dV4Pe(Orbit old, double PeR, double UT)
+		static Vector3d dV4Pe(Orbit old, double PeR, double UT)
 		{
 			var oldPeR = old.PeR;
 			var vel = old.getOrbitalVelocityAtUT(UT);
@@ -64,7 +68,13 @@ namespace ThrottleControlledAvionics
 				if(up && nPe > PeR || !up && nPe < PeR) max_dV = dV;
 				else min_dV = dV;
 			}
-			return (max_dV+min_dV)/2*pg;
+			return (max_dV+min_dV)/2*velN;
+		}
+
+		static Orbit new_PeR(Orbit old, double PeR, double UT)
+		{
+			var dV = dV4Pe(old, PeR, UT);
+			return new_orbit(old, dV, UT);
 		}
 
 		protected override void UpdateState()
@@ -79,27 +89,119 @@ namespace ThrottleControlledAvionics
 			if(enabled) Start = -1;
 		}
 
+		public class LandingSite
+		{
+			public readonly Orbit orbit;
+			public double TrueAnomaly { get; private set; }
+			public readonly double StartUT;
+			public readonly double TimeToSurface;
+			public readonly double AtSurfaceUT;
+			public readonly WayPoint SurfacePoint;
+
+			public LandingSite(Orbit landing_trajectory, double startUT)
+			{
+				orbit = landing_trajectory;
+				StartUT = startUT;
+				var tA0 = orbit.TrueAnomalyAtUT(startUT);
+				TrueAnomaly = orbit.TrueAnomalyAtRadius(orbit.referenceBody.Radius);
+				if(tA0 > TrueAnomaly) TrueAnomaly = TwoPi-TrueAnomaly;
+				SurfacePoint = MakeWaypoint(orbit, TrueAnomaly, "Landing Site");
+				TimeToSurface = orbit.GetDTforTrueAnomaly(TrueAnomaly, orbit.period/2);
+				AtSurfaceUT = StartUT+TimeToSurface;
+			}
+
+			public Vector3d GetRelSurfacePosition()
+			{ return orbit.referenceBody.GetRelSurfacePosition(SurfacePoint.Lat, SurfacePoint.Lon, 0); }
+
+			public Vector3d GetOrbitVelocityAtSurface()
+			{ return orbit.getOrbitalVelocityAtUT(StartUT+TimeToSurface); }
+
+			public override string ToString()
+			{
+				return string.Format("LandingSite:\nTrueAnomaly: {0} deg,\nCoordinates: {1},\nTimeToSurface: {2} s", 
+				                     TrueAnomaly*Mathf.Rad2Deg, SurfacePoint, TimeToSurface);
+			}
+		}
+
+		public const double TwoPi = 6.2831853;
+
 		protected override void Update()
 		{
 			if(!IsActive || VSL.orbit.referenceBody == null) return;
+			double dist = DEO.Dtol*10;
+			double old_dist = dist;
+			double rate = 0.1;
 			if(Start < 0) 
 			{
-				var dV = 0.0;
-				var ttb = 0f;
-				var offset = DEO.StartOffset;
-				do
-				{
-					if(ttb > 0) offset = ttb+DEO.StartOffset;
-					dV = dV4Pe(VSL.orbit, VSL.mainBody.Radius*DEO.PeR, VSL.Physics.UT+offset);
-					ttb = MAN.TTB((float)Math.Abs(dV), 1)/2;
-				} while(ttb > offset);
-
-				Start = VSL.Physics.UT+offset;
-				var node = VSL.vessel.patchedConicSolver.AddManeuverNode(Start);
-				node.DeltaV = new Vector3d(0,0,dV);
-				VSL.vessel.patchedConicSolver.UpdateFlightPlan();
+				Start = VSL.Physics.UT+DEO.StartOffset;
+				var PeR = DEO.PeR;
+				var target = MakeWaypoint(VSL.orbit, VSL.orbit.trueAnomaly+Mathf.PI*1.5, "Target");
+				CFG.Waypoints.Enqueue(target);
+				LandingSite site = null;
+				var maxI = 100;
+				do {
+					if(site != null) 
+					{
+						if(Math.Abs(old_dist) < Math.Abs(dist)) rate /=2;
+						Start = site.StartUT+dist/TwoPi*Body.rotationPeriod*rate;
+						if(Start-VSL.Physics.UT < DEO.StartOffset) 
+							Start = site.StartUT+Body.rotationPeriod/2;
+						Log("Changing start time: {0} s", Start-site.StartUT);
+					}
+					site = new LandingSite(new_PeR(VSL.orbit, VSL.mainBody.Radius*PeR, Start), Start);
+					var targetPos0  = Body.GetRelSurfacePosition(target.Lat, target.Lon, 0);
+					var targetPos1  = Quaternion.AngleAxis((float)((site.AtSurfaceUT-VSL.Physics.UT)/
+					                                               VSL.mainBody.rotationPeriod * Mathf.PI*2f), 
+					                                       Body.angularVelocity) * targetPos0;
+					old_dist = dist;
+					dist = target.AngleTo(site.SurfacePoint);
+					dist *= Math.Sign(Vector3d.Dot(targetPos1-site.GetRelSurfacePosition(), site.GetOrbitVelocityAtSurface()));
+					Log("{0}\ndistance: {1} m", site, dist*Body.Radius);
+					maxI--;
+				} while(Math.Abs(dist*Body.Radius) > DEO.Dtol && maxI > 0);
 			}
 		}
+
+		public static WayPoint MakeWaypoint(Orbit orb, double tA, string name = "")
+		{
+			var pos = orb.getPositionFromTrueAnomaly(tA);
+			var lat = orb.referenceBody.GetLatitude(pos);
+			var lon = orb.referenceBody.GetLongitude(pos);
+			var wp = new WayPoint(lat, lon);
+			if(!string.IsNullOrEmpty(name)) wp.Name = name;
+			return wp;
+		}
+
+		void addNode(Vector3d dV, double UT)
+		{
+			var node = VSL.vessel.patchedConicSolver.AddManeuverNode(UT);
+			var norm = O.GetOrbitNormal();
+			var radial = Vector3d.Cross(O.getOrbitalVelocityAtUT(UT), norm).normalized;
+			node.DeltaV = new Vector3d(Vector3d.Dot(dV, radial),
+			                           Vector3d.Dot(dV, norm),
+			                           Vector3d.Dot(dV, O.getOrbitalVelocityAtUT(UT).normalized));
+			VSL.vessel.patchedConicSolver.UpdateFlightPlan();
+		}
+
+//		void deorbit()
+//		{
+//			if(Start < 0) 
+//			{
+//				var dV = 0.0;
+//				var ttb = 0f;
+//				var offset = DEO.StartOffset;
+//				do {
+//					if(ttb > 0) offset = ttb+DEO.StartOffset;
+//					dV = dV4Pe(VSL.orbit, VSL.mainBody.Radius*DEO.PeR, VSL.Physics.UT+offset);
+//					ttb = MAN.TTB((float)Math.Abs(dV), 1)/2;
+//				} while(ttb > offset);
+//
+//				Start = VSL.Physics.UT+offset;
+//				var node = VSL.vessel.patchedConicSolver.AddManeuverNode(Start);
+//				node.DeltaV = new Vector3d(0,0,dV);
+//				VSL.vessel.patchedConicSolver.UpdateFlightPlan();
+//			}
+//		}
 	}
 }
 
