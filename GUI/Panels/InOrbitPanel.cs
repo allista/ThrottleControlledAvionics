0d28270324edd932d8dc12bde934203fa45f47f2
Allis Tauri <allista@gmail.com>
2016-03-03 15:27:03 +0300

    Moved all checks to Update in InOrbitPanel to prevent Unity GUI exceptions.

--- GUI/Panels/InOrbitPanel.cs
+++ GUI/Panels/InOrbitPanel.cs
@@ -22,19 +22,33 @@ namespace ThrottleControlledAvionics
 		DeorbitAutopilot DEO;
 		PointNavigator PN;
 
+		bool draw_MVA;
+		bool draw_DEO;
+		bool draw_WRP;
+		bool draw_MAP;
+		bool draw_PN;
+		bool draw;
+
+		public override void Update()
+		{
+			var tVessel = VSL.TargetVessel;
+			draw_MVA = MVA != null && (CFG.AP1.Any(Autopilot1.MatchVel, Autopilot1.MatchVelNear) || (VSL.InOrbit && tVessel != null && tVessel.situation == Vessel.Situations.ORBITING) && !CFG.AP1[Autopilot1.Maneuver]);
+			draw_DEO = DEO != null && (CFG.AP2[Autopilot2.Deorbit] || (VSL.InOrbit && tVessel != null && tVessel.LandedOrSplashed || VSL.Target is WayPoint) && !CFG.AP1[Autopilot1.Maneuver]);
+			draw_MAP = MAP != null && VSL.HasManeuverNode;
+			draw_WRP = WRP != null && (VSL.Info.Countdown > 0 || draw_MVA || draw_DEO || draw_MAP);
+			draw_PN  = PN  != null && !VSL.OnPlanet && !CFG.AP1[Autopilot1.Maneuver] && !CFG.AP2[Autopilot2.Deorbit];
+			draw = draw_MVA || draw_DEO || draw_MAP || draw_WRP || draw_PN;
+		}
+
 		public override void Draw()
 		{
-			if(!VSL.InOrbit || (MVA == null && MAP == null)) return;
+			if(!draw) return;
 			GUILayout.BeginHorizontal();
-			var tVessel = VSL.TargetVessel;
-			var MVA_aplicable = tVessel != null && tVessel.situation == Vessel.Situations.ORBITING && !CFG.AP1[Autopilot1.Maneuver];
-			var DEO_aplicable = (tVessel != null && tVessel.LandedOrSplashed || VSL.Target is WayPoint) && !CFG.AP1[Autopilot1.Maneuver];
-			if(WRP != null && (VSL.Info.Countdown > 0 || VSL.HasManeuverNode || VSL.HasTarget)) WRP.Draw();
-			if(MAP != null && VSL.HasManeuverNode) MAP.Draw();
-			if(PN != null && !CFG.AP1[Autopilot1.Maneuver] && !CFG.AP2[Autopilot2.Deorbit]) 
-				ThrottleControlledAvionics.NavigationControls.AddSingleWaypointInMapView();
-			if(DEO != null && DEO_aplicable) DEO.Draw();
-			if(MVA != null && MVA_aplicable) MVA.Draw();
+			if(draw_WRP) WRP.Draw();
+			if(draw_MAP) MAP.Draw();
+			if(draw_PN) ThrottleControlledAvionics.NavigationControls.AddSingleWaypointInMapView();
+			if(draw_DEO) DEO.Draw();
+			if(draw_MVA) MVA.Draw();
 			if(VSL.Info.Countdown >= 0)
 				GUILayout.Label(string.Format("Countdown: {0:F1}s", VSL.Info.Countdown), Styles.white, GUILayout.ExpandWidth(true));
 			if(VSL.Info.TTB >= 0)
