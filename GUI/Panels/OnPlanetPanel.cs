cc0f71ed5d05caf1f878ce390e0d287bbd99342f
Allis Tauri <allista@gmail.com>
2016-04-30 22:03:37 +0300

    Added RadarBeam in DEBUG mode.

--- GUI/Panels/OnPlanetPanel.cs
+++ GUI/Panels/OnPlanetPanel.cs
@@ -71,6 +71,9 @@ namespace ThrottleControlledAvionics
 								var alt = tca.GetModule<AltitudeControl>();
 								if(alt != null) alt.SetAltitudeAboveTerrain(CFG.AltitudeAboveTerrain);
 							});
+					#if DEBUG
+					RAD.RadarBeam();
+					#endif
 				}
 			}
 			if(CC != null)
