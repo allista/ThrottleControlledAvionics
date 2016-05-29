35771d1b5f7610653957a9fb41eafdf9857e46fc
Allis Tauri <allista@gmail.com>
2016-05-29 22:24:23 +0300

    Separated Extensions into Misc, Vector, Part, PartModule, Vessel and Collections; added code from Hangar.

--- Utils/Extensions.cs
+++ Utils/Extensions.cs
@@ -14,27 +14,60 @@ using UnityEngine;
 
 namespace ThrottleControlledAvionics
 {
-	public static class Extensions
+	public static class MiscExtensions
 	{
-		#region Logging
-		public static string Title(this Part p) { return p.partInfo != null? p.partInfo.title : p.name; }
-
-		public static void Log(this Part p, string msg, params object[] args)
+		public static Color Normalized(this Color c)
 		{
-			var vname = p.vessel == null? "" : p.vessel.vesselName;
-			var _msg = string.Format("{0}.{1} [{2}]: {3}", 
-			                         vname, p.name, p.flightID, msg);
-			Utils.Log(_msg, args);
+			var max = c.r > c.g? (c.r > c.b? c.r : c.b) : (c.g > c.b? c.g : c.b);
+			return max.Equals(0)? c : new Color(c.r / max, c.g / max, c.b / max);
+		}
+
+		#region From blizzy's Toolbar
+		public static Vector2 clampToScreen(this Vector2 pos) 
+		{
+			pos.x = Mathf.Clamp(pos.x, 0, Screen.width - 1);
+			pos.y = Mathf.Clamp(pos.y, 0, Screen.height - 1);
+			return pos;
+		}
+
+		public static Rect clampToScreen(this Rect rect) 
+		{
+			rect.width = Mathf.Clamp(rect.width, 0, Screen.width);
+			rect.height = Mathf.Clamp(rect.height, 0, Screen.height);
+			rect.x = Mathf.Clamp(rect.x, 0, Screen.width - rect.width);
+			rect.y = Mathf.Clamp(rect.y, 0, Screen.height - rect.height);
+			return rect;
+		}
+
+		public static Rect clampToWindow(this Rect rect, Rect window) 
+		{
+			rect.width = Mathf.Clamp(rect.width, 0, window.width);
+			rect.height = Mathf.Clamp(rect.height, 0, window.height);
+			rect.x = Mathf.Clamp(rect.x, 0, window.width - rect.width);
+			rect.y = Mathf.Clamp(rect.y, 0, window.height - rect.height);
+			return rect;
 		}
 		#endregion
 
-		public static void ForEach<T>(this IEnumerable<T> E, Action<T> action)
-		{
-			var en = E.GetEnumerator();
-			while(en.MoveNext()) action(en.Current);
-		}
+		#region ConfigNode
+		public static void AddRect(this ConfigNode n, string name, Rect r)
+		{ n.AddValue(name, ConfigNode.WriteQuaternion(new Quaternion(r.x, r.y, r.width, r.height))); }
 
-		#region Vector3
+		public static Rect GetRect(this ConfigNode n, string name)
+		{ 
+			try 
+			{ 
+				var q = ConfigNode.ParseQuaternion(n.GetValue(name)); 
+				return new Rect(q.x, q.y, q.z, q.w);
+			}
+			catch { return default(Rect); }
+		}
+		#endregion
+	}
+
+
+	public static class VectorExtensions
+	{
 		public static Vector3 CubeNorm(this Vector3 v)
 		{
 			if(v.IsZero()) return v;
@@ -47,18 +80,12 @@ namespace ThrottleControlledAvionics
 			return v/max;
 		}
 
-		public static Color Normalized(this Color c)
-		{
-			var max = c.r > c.g? (c.r > c.b? c.r : c.b) : (c.g > c.b? c.g : c.b);
-			return max.Equals(0)? c : new Color(c.r / max, c.g / max, c.b / max);
-		}
-
 		public static Vector3 Inverse(this Vector3 v, float nan=float.MaxValue) 
 		{ 
 			return new Vector3(
-			v.x.Equals(0)? nan : 1f/v.x, 
-			v.y.Equals(0)? nan : 1f/v.y, 
-			v.z.Equals(0)? nan : 1f/v.z); 
+				v.x.Equals(0)? nan : 1f/v.x, 
+				v.y.Equals(0)? nan : 1f/v.y, 
+				v.z.Equals(0)? nan : 1f/v.z); 
 		}
 
 		public static Vector3 ClampComponents(this Vector3 v, float min, float max) 
@@ -124,57 +151,88 @@ namespace ThrottleControlledAvionics
 
 		public static Vector3 MinComponent(this Vector3 v)
 		{ return v.Component(v.MinI()); }
-		#endregion
+	}
 
-		#region From blizzy's Toolbar
-		public static Vector2 clampToScreen(this Vector2 pos) 
+
+	public static class CollectionsExtensions
+	{
+		public static TSource SelectMax<TSource>(this IEnumerable<TSource> s, Func<TSource, float> metric)
 		{
-			pos.x = Mathf.Clamp(pos.x, 0, Screen.width - 1);
-			pos.y = Mathf.Clamp(pos.y, 0, Screen.height - 1);
-			return pos;
-		}
-
-		public static Rect clampToScreen(this Rect rect) 
-		{
-			rect.width = Mathf.Clamp(rect.width, 0, Screen.width);
-			rect.height = Mathf.Clamp(rect.height, 0, Screen.height);
-			rect.x = Mathf.Clamp(rect.x, 0, Screen.width - rect.width);
-			rect.y = Mathf.Clamp(rect.y, 0, Screen.height - rect.height);
-			return rect;
-		}
-
-		public static Rect clampToWindow(this Rect rect, Rect window) 
-		{
-			rect.width = Mathf.Clamp(rect.width, 0, window.width);
-			rect.height = Mathf.Clamp(rect.height, 0, window.height);
-			rect.x = Mathf.Clamp(rect.x, 0, window.width - rect.width);
-			rect.y = Mathf.Clamp(rect.y, 0, window.height - rect.height);
-			return rect;
-		}
-		#endregion
-
-		#region ConfigNode
-		public static void AddRect(this ConfigNode n, string name, Rect r)
-		{ n.AddValue(name, ConfigNode.WriteQuaternion(new Quaternion(r.x, r.y, r.width, r.height))); }
-
-		public static Rect GetRect(this ConfigNode n, string name)
-		{ 
-			try 
-			{ 
-				var q = ConfigNode.ParseQuaternion(n.GetValue(name)); 
-				return new Rect(q.x, q.y, q.z, q.w);
+			float max_v = -1;
+			TSource max_e = default(TSource);
+			foreach(TSource e in s)
+			{
+				float m = metric(e);
+				if(m > max_v) { max_v = m; max_e = e; }
 			}
-			catch { return default(Rect); }
+			return max_e;
 		}
-		#endregion
 
+		public static void ForEach<TSource>(this IEnumerable<TSource> E, Action<TSource> action)
+		{
+			var en = E.GetEnumerator();
+			while(en.MoveNext()) action(en.Current);
+		}
+
+		public static void ForEach<TSource>(this TSource[] a, Action<TSource> action)
+		{ for(int i = 0; i < a.Length; i++) action(a[i]); }
+
+		public static TSource Pop<TSource>(this LinkedList<TSource> l)
+		{
+			TSource e = l.Last.Value;
+			l.RemoveLast();
+			return e;
+		}
+
+		public static TSource Min<TSource>(params TSource[] args) where TSource : IComparable
+		{
+			if(args.Length == 0) throw new InvalidOperationException("Min: arguments list should not be empty");
+			TSource min = args[0];
+			foreach(var arg in args)
+			{ if(min.CompareTo(arg) < 0) min = arg; }
+			return min;
+		}
+
+		public static TSource Max<TSource>(params TSource[] args) where TSource : IComparable
+		{
+			if(args.Length == 0) throw new InvalidOperationException("Max: arguments list should not be empty");
+			TSource max = args[0];
+			foreach(var arg in args)
+			{ if(max.CompareTo(arg) > 0) max = arg; }
+			return max;
+		}
+
+		public static TValue Next<TKey, TValue>(this SortedList<TKey,TValue> list, TKey key)
+		{
+			try
+			{
+				var i = list.IndexOfKey(key);
+				var ni = (i+1) % list.Count;
+				return list.Values[ni];
+			} catch { return default(TValue); }
+		}
+
+		public static TValue Prev<TKey, TValue>(this SortedList<TKey,TValue> list, TKey key)
+		{
+			try
+			{
+				var i = list.IndexOfKey(key);
+				var ni = i > 0? i-1 : list.Count-1;
+				return list.Values[ni];
+			} catch { return default(TValue); }
+		}
+	}
+
+
+	public static class PartExtensions
+	{
+		#region from MechJeb2 PartExtensions
 		public static bool HasModule<T>(this Part p) where T : PartModule
-		{ return p.Modules.GetModules<T>().Any(); }
+		{ return p.Modules.OfType<T>().Any(); }
 
 		public static T GetModule<T>(this Part p) where T : PartModule
-		{ return p.Modules.GetModules<T>().FirstOrDefault(); }
+		{ return p.Modules.OfType<T>().FirstOrDefault(); }
 
-		#region From MechJeb2
 		public static bool IsPhysicallySignificant(this Part p)
 		{
 			bool physicallySignificant = (p.physicalSignificance != Part.PhysicalSignificance.NONE);
@@ -186,9 +244,177 @@ namespace ThrottleControlledAvionics
 			physicallySignificant &= !p.HasModule<ModuleLandingGear>() && !p.HasModule<LaunchClamp>();
 			return physicallySignificant;
 		}
-		#endregion
 
 		public static float TotalMass(this Part p) { return p.mass+p.GetResourceMass(); }
+		#endregion
+
+		#region Find Modules or Parts
+		public static Part RootPart(this Part p) 
+		{ return p.parent == null ? p : p.parent.RootPart(); }
+
+		public static List<Part> AllChildren(this Part p)
+		{
+			var all_children = new List<Part>{};
+			foreach(Part ch in p.children) 
+			{
+				all_children.Add(ch);
+				all_children.AddRange(ch.AllChildren());
+			}
+			return all_children;
+		}
+
+		public static List<Part> AllConnectedParts(this Part p)
+		{
+			if(p.parent != null) return p.parent.AllConnectedParts();
+			var all_parts = new List<Part>{p};
+			all_parts.AddRange(p.AllChildren());
+			return all_parts;
+		}
+
+		public static Part AttachedPartWithModule<T>(this Part p) where T : PartModule
+		{
+			if(p.parent != null && p.parent.HasModule<T>()) return p.parent;
+			foreach(var c in p.children) if(c.HasModule<T>()) return c;
+			return null;
+		}
+
+		public static List<ModuleT> AllModulesOfType<ModuleT>(this Part part, ModuleT exception = null)
+			where ModuleT : PartModule
+		{
+			var passages = new List<ModuleT>();
+			foreach(Part p in part.AllConnectedParts())
+				passages.AddRange(from m in p.Modules.OfType<ModuleT>()
+				                  where exception == null || m != exception
+				                  select m);
+			return passages;
+		}
+		#endregion
+
+		#region Resources and Phys-Props
+		public static float TotalCost(this Part p) { return p.partInfo != null? p.partInfo.cost : 0; }
+
+		public static float ResourcesCost(this Part p) 
+		{ 
+			return (float)p.Resources.Cast<PartResource>()
+				.Aggregate(0.0, (a, b) => a + b.amount * b.info.unitCost); 
+		}
+
+		public static float MaxResourcesCost(this Part p) 
+		{ 
+			return (float)p.Resources.Cast<PartResource>()
+				.Aggregate(0.0, (a, b) => a + b.maxAmount * b.info.unitCost); 
+		}
+
+		public static float DryCost(this Part p) { return p.TotalCost() - p.MaxResourcesCost(); }
+
+		public static float MassWithChildren(this Part p)
+		{
+			float mass = p.TotalMass();
+			p.children.ForEach(ch => mass += ch.MassWithChildren());
+			return mass;
+		}
+		#endregion
+
+		#region Actions
+		public static void BreakConnectedStruts(this Part p)
+		{
+			//break strut connectors
+			foreach(Part part in p.AllConnectedParts())
+			{
+				var s = part as StrutConnector;
+				if(s == null || s.target == null) continue;
+				if(s.parent == p || s.target == p)
+				{
+					s.BreakJoint();
+					s.targetAnchor.gameObject.SetActive(false);
+					s.direction = Vector3.zero;
+				}
+			}
+		}
+
+		public static void UpdateAttachedPartPos(this Part p, AttachNode node)
+		{
+			if(node == null) return;
+			var ap = node.attachedPart; 
+			if(ap == null) return;
+			var an = ap.findAttachNodeByPart(p);	
+			if(an == null) return;
+			var dp =
+				p.transform.TransformPoint(node.position) -
+				ap.transform.TransformPoint(an.position);
+			if(ap == p.parent) 
+			{
+				while (ap.parent) ap = ap.parent;
+				ap.transform.position += dp;
+				p.transform.position -= dp;
+			} 
+			else ap.transform.position += dp;
+		}
+		#endregion
+
+		#region Logging
+		public static string Title(this Part p) { return p.partInfo != null? p.partInfo.title : p.name; }
+
+		public static void Log(this Part p, string msg, params object[] args)
+		{
+			var vname = p.vessel == null? "" : p.vessel.vesselName;
+			var _msg = string.Format("{0}.{1} [{2}]: {3}", 
+			                         vname, p.name, p.flightID, msg);
+			Utils.Log(_msg, args);
+		}
+		#endregion
+
+		#region Misc
+		//directly from Part disassembly
+		public static PartModule.StartState StartState(this Part part)
+		{
+			var _state = PartModule.StartState.None;
+			if(HighLogic.LoadedSceneIsEditor)
+				_state |= PartModule.StartState.Editor;
+			else if(HighLogic.LoadedSceneIsFlight)
+			{
+				if(part.vessel.situation == Vessel.Situations.PRELAUNCH)
+				{
+					_state |= PartModule.StartState.PreLaunch;
+					_state |= PartModule.StartState.Landed;
+				}
+				if(part.vessel.situation == Vessel.Situations.DOCKED)
+					_state |= PartModule.StartState.Docked;
+				if(part.vessel.situation == Vessel.Situations.ORBITING ||
+				   part.vessel.situation == Vessel.Situations.ESCAPING)
+					_state |= PartModule.StartState.Orbital;
+				if(part.vessel.situation == Vessel.Situations.SUB_ORBITAL)
+					_state |= PartModule.StartState.SubOrbital;
+				if(part.vessel.situation == Vessel.Situations.SPLASHED)
+					_state |= PartModule.StartState.Splashed;
+				if(part.vessel.situation == Vessel.Situations.FLYING)
+					_state |= PartModule.StartState.Flying;
+				if(part.vessel.situation == Vessel.Situations.LANDED)
+					_state |= PartModule.StartState.Landed;
+			}
+			return _state;
+		}
+		#endregion
+	}
+
+
+	public static class PartModuleExtensions
+	{
+		public static string Title(this PartModule pm) 
+		{ return pm.part.partInfo != null? pm.part.partInfo.title : pm.part.name; }
+
+		public static void EnableModule(this PartModule pm, bool enable)
+		{ pm.enabled = pm.isEnabled = enable; }
+	}
+
+
+	public static class VesselExtensions
+	{
+		public static Part GetPart<T>(this Vessel v) where T : PartModule
+		{ return v.parts.FirstOrDefault(p => p.HasModule<T>()); }
+
+		public static bool PartsStarted(this Vessel v)
+		{ return v.parts.TrueForAll(p => p.started); }
 
 		public static Bounds EnginesExhaust(this Vessel vessel)
 		{
