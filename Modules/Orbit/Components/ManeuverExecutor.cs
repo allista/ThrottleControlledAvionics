011850a34e52a0898d126fe0a44d01ac80148e92
Allis Tauri <allista@gmail.com>
2016-05-31 18:01:31 +0300

    Moved Executors to Components subfolder. Added ToOrbitAutopilot to TechTree.

---
+++ Modules/Orbit/Components/ManeuverExecutor.cs
@@ -0,0 +1,64 @@
+﻿//  Author:
+//       Allis Tauri <allista@gmail.com>
+//
+//  Copyright (c) 2016 Allis Tauri
+//
+// This work is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License. 
+// To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/ 
+// or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+//
+using System;
+using UnityEngine;
+
+namespace ThrottleControlledAvionics
+{
+	public class ManeuverExecutor : TCAComponent
+	{
+		ThrottleControl THR;
+		TranslationControl TRA;
+		AttitudeControl ATC;
+
+		public delegate bool ManeuverCondition(float dV);
+		readonly FuzzyThreshold<double> dVrem = new FuzzyThreshold<double>(1, 0.5f);
+
+		public ManeuverExecutor(ModuleTCA tca) : base(tca) { InitModuleFields(); }
+
+		public void Reset() { dVrem.Value = dVrem.Upper+1; }
+
+		public bool Execute(Vector3d dV, float MinDeltaV = 0.1f, ManeuverCondition condition = null)
+		{
+			dVrem.Value = dV.magnitude;
+			//end if below the minimum dV
+			if(dVrem < MinDeltaV) return false;
+			THR.Throttle = 0;
+			VSL.Engines.ActivateNextStageOnFlameout();
+			VSL.Engines.ActivateEnginesIfNeeded();
+			//orient along the burning vector
+			if(dVrem && VSL.Controls.RCSAvailableInDirection(-dV)) 
+				CFG.AT.OnIfNot(Attitude.KillRotation);
+			else 
+			{
+				CFG.AT.OnIfNot(Attitude.Custom);
+				ATC.SetThrustDirW(-dV);
+			}
+			//check the condition
+			if(condition != null && !condition((float)dVrem)) return true;
+			if(VSL.Controls.TranslationAvailable)
+			{
+				if(dVrem || ATC.AttitudeError > GLB.ATCB.AttitudeErrorThreshold)
+					TRA.AddDeltaV(-VSL.LocalDir(dV));
+				if(dVrem && CFG.AT[Attitude.KillRotation]) 
+				{
+					var errorF = Utils.ClampL(Vector3.Dot(VSL.Engines.Thrust.normalized, -dV.normalized), 0);
+					THR.DeltaV = (float)dVrem * errorF*errorF;
+				}
+				else THR.DeltaV = (float)dVrem;
+			}
+			else THR.DeltaV = (float)dVrem;
+//			LogF("\ndVrem: {}\nAttitudeError {}, DeltaV: {}, Throttle {}, RCS {}", 
+//			     dVrem, ATC.AttitudeError, THR.DeltaV, THR.Throttle, may_use_RCS);//debug
+			return true;
+		}
+	}
+}
+
---
+++ Modules/Orbit/Components/ToOrbitExecutor.cs
@@ -0,0 +1,115 @@
+﻿//  Author:
+//       Allis Tauri <allista@gmail.com>
+//
+//  Copyright (c) 2016 Allis Tauri
+//
+// This work is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License. 
+// To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/ 
+// or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+//
+using System;
+using UnityEngine;
+
+namespace ThrottleControlledAvionics
+{
+	public class ToOrbitExecutor : TCAComponent
+	{
+		const double MinClimbTime = 5;
+
+		ThrottleControl THR;
+		BearingControl BRC;
+
+		readonly SingleAction GearAction = new SingleAction();
+		readonly FuzzyThreshold<double> ErrorThreshold = new FuzzyThreshold<double>();
+		readonly ManeuverExecutor Executor;
+
+		Vector3d target;
+		public Vector3d Target 
+		{ 
+			get { return target; } 
+			set { target = value; TargetR = target.magnitude; } 
+		}
+		public double TargetR { get; private set; }
+
+		public double LaunchUT;
+		public double ApAUT;
+
+		Orbit VesselOrbit { get { return VSL.vessel.orbitDriver.orbit; } }
+		CelestialBody Body { get { return VesselOrbit.referenceBody; } }
+		Vector3d hV(double UT) { return TrajectoryCalculator.hV(VesselOrbit, UT); }
+		public double dApA { get; private set; }
+
+		public ToOrbitExecutor(ModuleTCA tca) : base(tca) 
+		{ 
+			InitModuleFields();
+			Executor = new ManeuverExecutor(tca);
+			GearAction.action = () =>VSL.GearOn(false);
+			ErrorThreshold.Lower = 2*GLB.ORB.Dtol;
+			ErrorThreshold.Upper = 10*GLB.ORB.Dtol;
+		}
+
+		public bool Liftoff()
+		{
+			VSL.Engines.ActivateEnginesIfNeeded();
+			if(VSL.VerticalSpeed.Absolute/VSL.Physics.G < MinClimbTime)
+			{ 
+				Status("Liftoff...");
+				CFG.DisableVSC();
+				CFG.VTOLAssistON = true;
+				THR.Throttle = 1;
+				return true;
+			}
+			GearAction.Run();
+			CFG.VTOLAssistON = false;
+			CFG.StabilizeFlight = false;
+			CFG.HF.Off();
+			return false;
+		}
+
+		public bool GravityTurn(double gturn_curve, double dist2vel, double Dtol)
+		{
+			dApA = TargetR-VesselOrbit.ApR;
+			var vel   = Vector3d.zero;
+			var cApV  = VesselOrbit.getRelativePositionAtUT(VSL.Physics.UT+VesselOrbit.timeToAp);
+			var hv    = Vector3d.Exclude(VesselOrbit.pos, VesselOrbit.vel).normalized;
+			var alpha = Utils.ProjectionAngle(cApV, target, Vector3d.Cross(VesselOrbit.GetOrbitNormal(), cApV))*Mathf.Deg2Rad*Body.Radius;
+			ErrorThreshold.Value = dApA+alpha;
+			if(!ErrorThreshold)
+			{
+				if(alpha > Dtol)
+				{
+					var hvel = Utils.ClampL(alpha-dApA*gturn_curve, 0)*dist2vel*
+						Utils.Clamp((VesselOrbit.ApA-VSL.Altitude.Absolute)/100, 0, 1);
+					if(Body.atmosphere) hvel *= Math.Sqrt(Utils.Clamp(VSL.Altitude.Absolute/Body.atmosphereDepth, 0, 1));
+					vel += hv*hvel;
+				}
+				if(dApA > Dtol)
+					vel += VSL.Physics.Up.xzy*dApA*gturn_curve*dist2vel;
+				vel *= VSL.Physics.StG/Utils.G0;
+				if(!vel.IsZero())
+				{
+					var norm = VesselOrbit.GetOrbitNormal();
+					vel += norm*Math.Sin((90-Vector3d.Angle(norm, target))*Mathf.Deg2Rad)*vel.magnitude*
+						Utils.Clamp(VSL.VerticalSpeed.Absolute/VSL.Physics.G-MinClimbTime, 0, 100);
+				}
+				vel = vel.xzy;
+				if(Executor.Execute(vel, 1)) 
+				{
+					if(CFG.AT.Not(Attitude.KillRotation)) 
+					{
+						CFG.BR.OnIfNot(BearingMode.Auto);
+						BRC.ForwardDirection = hV(VSL.Physics.UT).xzy;
+					}
+					Status("Gravity turn...");
+					return true;
+				}
+			}
+			CFG.BR.OffIfOn(BearingMode.Auto);
+			Status("Coasting...");
+			CFG.AT.OnIfNot(Attitude.KillRotation);
+			THR.Throttle = 0;
+			return (Body.atmosphere && VesselOrbit.radius < Body.Radius+Body.atmosphereDepth);
+		}
+	}
+}
+
