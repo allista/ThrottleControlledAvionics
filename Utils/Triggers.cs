b7f3c72bfbfc57922a3a8807f3906238039eff1d
Allis Tauri <allista@gmail.com>
2016-06-13 01:17:51 +0300

    Factored out TimerBase<T>; added Timer class that uses UT and RealTimer that uses system time.
    Added Timer.Remaining property that returns remaining seconds.
    Added action field to TimerBase and SingleAction.

--- Utils/Triggers.cs
+++ Utils/Triggers.cs
@@ -12,52 +12,37 @@ using System;
 namespace ThrottleControlledAvionics
 {
 	#region Timers
-	public class TimerBase
+	public abstract class TimerBase<T> where T : IComparable
 	{
-		protected DateTime next_time;
+		protected T next_time;
 		public double Period;
+		public Action action;
 
-		public TimerBase(double period) { Period = period; next_time = DateTime.MinValue; }
-		public void Reset() { next_time = DateTime.MinValue; }
-
-		public override string ToString()
-		{
-			var time = DateTime.Now;
-			return string.Format("time: {0} < next time {1}: {2}; remaining: {3}", 
-			                     time, next_time, time < next_time, next_time-time);
-		}
+		protected TimerBase(double period) { Period = period; }
+		public abstract void Reset();
 	}
 
-	public class ActionDamper : TimerBase
+	public abstract class Timer<T> : TimerBase<T> where T : IComparable
 	{
-		public ActionDamper(double period = 0.1) : base(period) {}
+		protected abstract T now { get; }
+		protected abstract T default_time { get; }
 
-		public void Run(Action action)
-		{
-			var time = DateTime.Now;
-			if(next_time > time) return;
-			next_time = time.AddSeconds(Period);
-			action();
-		}
-	}
-
-	public class Timer : TimerBase
-	{
-		public Timer(double period = 1) : base(period) {}
-
-		public void Start() { next_time = DateTime.Now.AddSeconds(Period); }
+		protected Timer(double period) : base(period) { next_time = default_time; }
+		public override void Reset() { next_time = default_time; }
+		public abstract void Start();
+		public abstract double Remaining { get; }
 
 		public bool Check
 		{
 			get 
 			{
-				var time = DateTime.Now;
-				if(next_time == DateTime.MinValue)
+				var time = now;
+				if(next_time.Equals(default_time))
 				{
-					next_time = time.AddSeconds(Period); 
+					Start();
 					return false;
 				}
-				return next_time < time;
+				return next_time.CompareTo(time) < 0;
 			}
 		}
 
@@ -67,6 +52,49 @@ namespace ThrottleControlledAvionics
 			else Reset(); 
 			return false;
 		}
+		public bool RunIf(bool predicate) { return RunIf(action, predicate); }
+
+		public override string ToString()
+		{
+			var time = now;
+			return string.Format("time: {0} < next time {1}: {2}", 
+			                     time, next_time, time.CompareTo(next_time) < 0);
+		}
+	}
+
+	public class ActionDamper : TimerBase<DateTime>
+	{
+		public ActionDamper(double period = 0.1) : base(period) { next_time = DateTime.MinValue; }
+
+		public override void Reset() { next_time = DateTime.MinValue; }
+
+		public void Run(Action action)
+		{
+			var time = DateTime.Now;
+			if(next_time > time) return;
+			next_time = time.AddSeconds(Period);
+			action();
+		}
+
+		public void Run() { Run(action); }
+	}
+
+	public class RealTimer : Timer<DateTime>
+	{
+		protected override DateTime now { get { return DateTime.Now; } }
+		protected override DateTime default_time { get { return DateTime.MinValue; } }
+		public RealTimer(double period = 1) : base(period) { next_time = default_time; }
+		public override void Start() { next_time = now.AddSeconds(Period); }
+		public override double Remaining { get { return next_time.Subtract(now).TotalSeconds; } }
+	}
+
+	public class Timer : Timer<double>
+	{
+		protected override double now { get { return Planetarium.GetUniversalTime(); } }
+		protected override double default_time { get { return -1; } }
+		public Timer(double period = 1) : base(period) { next_time = default_time; }
+		public override void Start() { next_time = now+Period; }
+		public override double Remaining { get { return next_time-now; } }
 	}
 	#endregion
 
@@ -89,11 +117,10 @@ namespace ThrottleControlledAvionics
 		bool done;
 		public Action action;
 
-		public void Run() 
+		public void Run(Action action) 
 		{ if(!done) { action(); done = true; } }
 
-		public void Run(Action act) 
-		{ if(!done) { act(); done = true; } }
+		public void Run() { Run(action); }
 
 		public void Reset() { done = false; }
 
