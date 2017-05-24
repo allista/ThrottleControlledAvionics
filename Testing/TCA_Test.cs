0fd07ada8a497f6ea61c94287f4bba7ce87ad3b0
Allis Tauri <allista@gmail.com>
2017-05-24 19:17:39 +0300

    Adde base TCA_Test, DEO and REN tests. Removed obsolete files.

---
+++ Testing/TCA_Test.cs
@@ -0,0 +1,38 @@
+﻿//   TCA_Test.cs
+//
+//  Author:
+//       Allis Tauri <allista@gmail.com>
+//
+//  Copyright (c) 2017 Allis Tauri
+using System;
+using AT_Utils;
+
+namespace ThrottleControlledAvionics
+{
+    public abstract class TCA_Test : ITestScenario
+    {
+        static Globals GLB { get { return Globals.Instance; } }
+
+        protected ModuleTCA TCA;
+        protected VesselWrapper VSL { get { return TCA.VSL; } }
+        protected VesselConfig CFG { get { return TCA.CFG; } }
+
+        protected bool GetTCA()
+        {
+            if(FlightGlobals.ActiveVessel != null)
+                TCA = ModuleTCA.EnabledTCA(FlightGlobals.ActiveVessel);
+            return TCA != null;
+        }
+
+        #region ITestScenario implementation
+        public abstract string Setup();
+        public abstract bool Update(Random RND);
+        public abstract void Cleanup();
+
+        public virtual string Status { get; protected set; } = "";
+        public abstract bool NeedsFixedUpdate { get; }
+        public abstract bool NeedsUpdate { get; }
+        #endregion
+    }
+}
+
---
+++ VesselProps/StagingModel.cs
@@ -0,0 +1,74 @@
+﻿//   StagingModel.cs
+//
+//  Author:
+//       Allis Tauri <allista@gmail.com>
+//
+//  Copyright (c) 2017 Allis Tauri
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ThrottleControlledAvionics
+{
+    public class StagingModel : VesselProps
+    {
+        public class Stage : VesselProps, IShipconstruct
+        {
+            public int InverseIndex;
+
+            public List<Part> Parts { get; private set; }
+            public List<EngineWrapper> Engines { get; private set; }
+
+            public Stage(VesselWrapper vsl) : base(vsl) {}
+
+            public double M { get; private set; }
+
+            public double DeltaV(double Ve, double fuel_mass) { return Ve*Math.Log(M/(M-fuel_mass)); }
+
+            public EnginesStats GetEnginesStats()
+            {
+                var stats = new EnginesStats(VSL);
+                for(int i = 0, count = Engines.Count; i < count; i++)
+                {
+                    var e = Engines[i];
+                    e.InitState();
+                    e.InitTorque(VSL, GLB.ENG.TorqueRatioFactor);
+                    var throttle = e.Role == TCARole.MANUAL ? e.thrustLimit : 1;
+                    if(throttle > 0)
+                    {
+                        var thrust = e.nominalCurrentThrust(throttle);
+                        if(e.Role != TCARole.MANEUVER)
+                        {
+                            stats.MaxThrust += e.wThrustDir * thrust;
+                            stats.MaxDefThrust += e.defThrustDir * thrust;
+                            stats.MaxMassFlow += e.MaxFuelFlow * throttle;
+                            //stats.MaxFuelMass += ;
+                        }
+                        if(e.isSteering) 
+                            stats.TorqueLimits.Add(e.specificTorque*thrust);
+                    }
+                }
+                stats.MaxDeltaV = DeltaV(stats.MaxThrust.magnitude/stats.MaxMassFlow, stats.MaxFuelMass);
+                stats.Update();
+                return stats;
+            }
+        }
+
+        public StagingModel(VesselWrapper vsl) : base(vsl) {}
+
+        /// <summary>
+        /// The list of stages, from current to the last available, i.e. from greater inverse index to 0.
+        /// </summary>
+        public List<Stage> Stages = new List<Stage>();
+
+        public List<EnginesStats> GetEnginesStats()
+        { return Stages.Select(s => s.GetEnginesStats()).ToList(); }
+
+        public void UpdateModel()
+        {
+            
+        }
+    }
+}
+
