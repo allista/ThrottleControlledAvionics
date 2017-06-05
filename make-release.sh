79f9c94a6503672f61f1706dc7b0d3e723f286d9
Allis Tauri <allista@gmail.com>
2017-06-05 16:29:25 +0300

    Excluded .pdb files from archive.

--- make-release.sh
+++ make-release.sh
@@ -3,7 +3,7 @@
 cd $(dirname "$0")
 
 ../../PyKSPutils/make_mod_release \
--e '*/config.xml' '*/TCA.conf' '*/TCA.macro' '*.user' '*.orig' '*.mdb' \
+-e '*/config.xml' '*/TCA.conf' '*/TCA.macro' '*.user' '*.orig' '*.mdb' '*.pdb' \
 '*/001_AnisotropicPartResizer*' \
 '*/002_MultiAnimators.dll' \
 '*/AnimatedConverters.dll' \
