c46b08dd4abb2740cd477991be75fb35a9a51cb7
player <allista@gmail.com>
2017-08-20 11:59:00 +0300

    Added simulate_random_attitude; using PID3 in ATC.
    Added x-axis synchronization to plots produced by analyze_results.

--- Tests/ATC-sandbox.py
+++ Tests/ATC-sandbox.py
@@ -4,7 +4,8 @@ from datetime import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 
-from common import dt, clampL, clampH, clamp, PID, PID2, plt_show_maxed, color_grad, Filter, fit_plot, SimpleKalman
+from common import dt, clampL, clampH, clamp, PID, PID2, plt_show_maxed, color_grad, Filter, fit_plot, SimpleKalman, \
+    PID3
 from Engine import Engine
 
 class ATC(object):
@@ -43,8 +44,13 @@ class ATC(object):
         self.WheelsRatio = self.WheelsMaxAA/self.MaxAA
         self.InstantRatio = 1.0 if self.instant else self.WheelsMaxAA/self.MaxAA
         self.on_update = on_update
+        self.time = 0
         self.AV = 0
         self.AA = 0
+
+    @property
+    def errorF(self):
+        return abs(self.error/np.pi)
 
     def reset(self):
         self.AV = 0
@@ -54,17 +60,24 @@ class ATC(object):
         self.engineR.limit = self.base_level
         self.engineR.thrust = self.engineF.maxThrust * self.base_level
 
+    def _error1(self, alpha):
+        return 1-alpha + np.random.rand()*alpha*2
+
+    def _error2(self, alpha):
+        return (0.5-np.random.rand())*alpha*2
+
     def updateAV(self):
+        action = self.avPID.action
         if self.avPID.action > 0:
-            self.engineF.limit = self.base_level
-            self.engineR.limit = max(self.base_level - self.avPID.action, 0)
+            self.engineF.limit = min(self.base_level + action, 1)
+            self.engineR.limit = max(self.base_level - action, 0)
         else:
-            self.engineR.limit = self.base_level
-            self.engineF.limit = max(self.base_level + self.avPID.action, 0)
+            self.engineR.limit = min(self.base_level - action, 1)
+            self.engineF.limit = max(self.base_level + action, 0)
         self.engineF.update()
         self.engineR.update()
-        self.AA = (self.engineF.torque - self.engineR.torque + self.wheels*self.avPID.action) / self.MoI
-        self.AV += self.AA * dt + (np.random.rand()-0.5)*1e-4
+        self.AA = ((self.engineF.torque - self.engineR.torque) + self.wheels*self.avPID.action) / self.MoI
+        self.AV += self.AA * dt + (np.random.rand()-0.5)*1e-3
 
     def update(self):
         if self.on_update is not None:
@@ -79,15 +92,15 @@ class ATC(object):
         elif self.error < -np.pi: self.error += self.twoPi
 
     def simulate_constant_angular_velocity(self, needed_av, end_time, end_on_zero=False):
-        t = 0
+        self.time = 0
         time = [0]
         error = [needed_av]
         action = [self.avPID.action]
         thrust = [(self.engineF.thrust-self.engineR.thrust) / self.engineF.maxThrust]
         zero_stats = None
         self.reset()
-        while t < end_time:
-            t += dt
+        while self.time < end_time:
+            self.time += dt
             av_error = needed_av-self.AV
             if self.on_update is not None:
                 self.on_update(self)
@@ -95,54 +108,80 @@ class ATC(object):
                 self.avPID.update(av_error)
             self.updateAV()
             if not zero_stats and (av_error < self.tenth_deg or av_error*error[-1] < 0):
-                zero_stats = self.ZeroStats(t, abs(self.AA), 'AA', 'rad/s2')
+                zero_stats = self.ZeroStats(self.time, abs(self.AA), 'AA', 'rad/s2')
             error.append(av_error)
             action.append(self.avPID.action)
             thrust.append((self.engineF.thrust-self.engineR.thrust) / self.engineF.maxThrust)
-            time.append(t)
+            time.append(self.time)
             if zero_stats and end_on_zero: break
         return 'dAV [AA %.2f]' % self.MaxAA, time, np.fromiter(error, float)/np.pi*180, action, thrust, zero_stats
 
     def simulate_static_attitude(self, start_error, end_time, end_on_zero=False):
         self.error = start_error / 180.0 * np.pi
-        t = 0
+        self.time = 0
         time = [0]
         error = [self.error]
         action = [self.avPID.action]
         thrust = [self.engineF.limit]
         zero_stats = None
         self.reset()
-        while t < end_time:
-            t += dt
+        while self.time < end_time:
+            self.time += dt
             self.update()
             if not zero_stats and (self.error < self.tenth_deg or self.error*error[-1] < 0):
-                zero_stats = self.ZeroStats(t, abs(self.AV+self.AA*dt), 'AV', 'rad/s')
+                zero_stats = self.ZeroStats(self.time, abs(self.AV+self.AA*dt), 'AV', 'rad/s')
             error.append(self.error)
             action.append(self.avPID.action)
             thrust.append((self.engineF.thrust-self.engineR.thrust) / self.engineF.maxThrust)
-            time.append(t)
+            time.append(self.time)
         return 'dAng [AA %.2f]' % self.MaxAA, time, np.fromiter(error, float)/np.pi*180, action, thrust, zero_stats
 
     def simulate_linear_attitude(self, start_error, error_change_rate, end_time, end_on_zero=False):
         self.error = start_error / 180.0 * np.pi
         error_change_rate *= np.pi/180.0
-        t = 0
+        self.time = 0
         time = [0]
         error = [self.error]
         action = [self.atPID.action]
         thrust = [self.engineF.limit]
         zero_stats = None
         self.reset()
-        while t < end_time:
-            t += dt
+        while self.time < end_time:
+            self.time += dt
             self.error += error_change_rate * dt
             self.update()
             if not zero_stats and (self.error < self.tenth_deg or self.error*error[-1] < 0):
-                zero_stats = self.ZeroStats(t, abs(self.AV+self.AA*dt), 'AV', 'rad/s')
+                zero_stats = self.ZeroStats(self.time, abs(self.AV+self.AA*dt), 'AV', 'rad/s')
             error.append(self.error)
-            action.append(self.atPID.action)
+            action.append(self.avPID.action)
             thrust.append((self.engineF.thrust-self.engineR.thrust) / self.engineF.maxThrust)
-            time.append(t)
+            time.append(self.time)
+        return 'dAng [AA %.2f]' % self.MaxAA, time, np.fromiter(error, float)/np.pi*180, action, thrust, zero_stats
+
+    def simulate_random_attitude(self, start_error, error_change_rate, error_change_time, end_time, end_on_zero=False):
+        self.error = start_error / 180.0 * np.pi
+        error_change_rate *= np.pi/180.0
+        time_to_change = error_change_time
+        self.time = 0
+        time = [0]
+        error = [self.error]
+        action = [self.atPID.action]
+        thrust = [self.engineF.limit]
+        zero_stats = None
+        self.reset()
+        while self.time < end_time:
+            self.time += dt
+            time_to_change -= dt*np.random.rand()
+            if time_to_change < 0:
+                self.error += error_change_rate * (np.random.rand()-0.5) * 2
+                time_to_change = error_change_time
+            self.update()
+            if not zero_stats and (self.error < self.tenth_deg or self.error*error[-1] < 0):
+                zero_stats = self.ZeroStats(self.time, abs(self.AV+self.AA*dt), 'AV', 'rad/s')
+            error.append(self.error)
+            action.append(self.avPID.action)
+            thrust.append((self.engineF.thrust-self.engineR.thrust) / self.engineF.maxThrust)
+            time.append(self.time)
         return 'dAng [AA %.2f]' % self.MaxAA, time, np.fromiter(error, float)/np.pi*180, action, thrust, zero_stats
 
     def optimize_av_PID(self, needed_av, aa_threshold, step, end_time, P, I, D):
@@ -209,14 +248,14 @@ class ATC(object):
             if zero_stats:
                 print '\n%s\n' % str(zero_stats)
             print '=' * 80
-            plt.subplot(3, cols, col)
+            ax = plt.subplot(3, cols, col)
             plt.plot(time, error, label=name, color=colors[i])
             plt.ylabel('error (deg)')
             plt.legend()
-            plt.subplot(3, cols, cols+col)
+            plt.subplot(3, cols, cols+col, sharex=ax)
             plt.plot(time, action, color=colors[i])
             plt.ylabel('action')
-            plt.subplot(3, cols, cols*2+col)
+            plt.subplot(3, cols, cols*2+col, sharex=ax)
             plt.plot(time, thrust, color=colors[i])
             plt.xlabel('time')
             plt.ylabel('thrust')
@@ -330,10 +369,10 @@ if __name__ == '__main__':
         avI_Scale = 0.4
 
     class MixedConfig(object):
-        avP_A = 50
+        avP_A = 100
         avP_B = 0.04
-        avP_C = -50
-        avP_D = 0.8
+        avP_C = -100
+        avP_D = 0.7
 
         avD_A = 0.65
         avD_B = -0.01
@@ -342,7 +381,8 @@ if __name__ == '__main__':
 
         avI_Scale = 0.03
 
-    avFilter = Filter(0.1)
+    avFilter = Filter(0.8)
+    avFilter.setTau(3*dt)
 
     def tune_steering_mixed(atc, iErrf, imaxAA, AM):
         if atc.InstantRatio > 0.3:
@@ -362,27 +402,41 @@ if __name__ == '__main__':
             #     atc.avPID.P = 1/clampL(abs(AM)/12, 1)
             #     atc.avPID.I = 0.001
             #     atc.avPID.D = 1.4/atc.MaxAA**0.3
-            atc.atPID.P = 1
+
+            # clampH(abs(40*(abs(atc.avPID.perror)+abs(atc.error/np.pi)))**6, 1)
+            noise_scale = clamp((50 * (abs(atc.AV) + atc.errorF)) ** 0.6, 0.001, 1)
+
+            atc.atPID.P = 1#clamp(0.5*atc.MaxAA**0.5, 0.0001, 1)
             atc.atPID.I = 0
             atc.atPID.D = 0
-            noise_scale = clamp((50*(abs(atc.AV)+abs(atc.error/np.pi)))**0.5, 0.001, 1)
+
             atc.avPID.P = ((MixedConfig.avP_A / (atc.InstantRatio ** MixedConfig.avP_D + MixedConfig.avP_B) +
-                           MixedConfig.avP_C) / clampL(abs(AM), 1) / atc.MaxAA) \
-                          * noise_scale #clamp(abs(300*(abs(atc.avPID.perror)+abs(atc.error/np.pi)))**0.5, 0.1, 1)
+                           MixedConfig.avP_C)) / clampL(abs(AM), 1) / atc.MaxAA * noise_scale
             atc.avPID.D = ((MixedConfig.avD_A / (atc.InstantRatio ** MixedConfig.avD_D + MixedConfig.avD_B) +
-                           MixedConfig.avD_C) / atc.MaxAA) \
-                          * noise_scale #clampH(abs(40*(abs(atc.avPID.perror)+abs(atc.error/np.pi)))**6, 1)
-            atc.avPID.I = MixedConfig.avI_Scale*clampH(atc.MaxAA, 1) \
-                          * noise_scale
+                            MixedConfig.avD_C)) / atc.MaxAA * noise_scale
+            atc.avPID.I = MixedConfig.avI_Scale * clampH(atc.MaxAA, 1) * noise_scale
+
+            # atc.avPID.P = (1 + 8 / atc.MaxAA) * noise_scale
+            # atc.avPID.D = clampH((clampL(abs(AM) / atc.MaxAA, 1) ** 0.1 - 1), 2) * noise_scale
+
+
             # avFilter.ratio = clamp((abs(atc.avPID.perror)+abs(atc.error/np.pi))/atc.InstantRatio*50, 0.1, 1)
             # AV = avFilter.EWA(atc.AV)
             # print avFilter.ratio
             AV = atc.AV
             update_pids(atc, AV)
 
-    wheels_ratio = 0.06
-    AAs = 0.3, 0.7, 0.9, 1, 1.9, 3, 9
+    IRt = [(0.299, 0.5),
+           (0.2, 0.35),
+           (0.1, 0.2),
+           (0.05, 0.1),
+           (0.01, 0.3),
+           ]
+
+    wheels_ratio = 0.2
+    AAs = 0.3, 0.7, 0.9, 1, 1.9, 3, 9, 20
     # AAs = 1, 1.9, 3, 9
+    AAs = 2,
     angles = 85, 25, 3
 
     class SlowConfig(object):
@@ -422,7 +476,7 @@ if __name__ == '__main__':
         atc.atPID.update2(abs(atc.error), -AV)
         avErr = abs(atc.atPID.action) * np.sign(atc.error) - AV
         atc.avPID.update(avErr)
-        atc.avPID.action = clamp(atc.avPID.action, -1, 1)
+        atc.avPID.action = avFilter.EWA(clamp(atc.avPID.action, -1, 1))
 
     def tune_steering(atc):
         """
@@ -438,15 +492,14 @@ if __name__ == '__main__':
             tune_steering_mixed(atc, iErrf, imaxAA, AM)
         else:
             tune_steering_slow(atc, iErrf, imaxAA, AM)
-        print ('atc.MaxAA %f, err %f, av %f, am %f, iErr %f\natPID %s\navPID %s' %
-               (atc.MaxAA, atc.error / np.pi * 180, atc.AV / np.pi * 180, AM, iErrf, atc.atPID,
-                atc.avPID))
+        print ('time %f, atc.MaxAA %f, err %f, av %f, am %f, iErr %f\natPID %s\navPID %s' %
+               (atc.time, atc.MaxAA, atc.error / np.pi * 180, atc.AV / np.pi * 180, AM, iErrf, atc.atPID, atc.avPID))
 
     lever = 4
 
-    def simAA(error, maxAA, engine, base_thrust, wheels_ratio, error_rate=0):
+    def simAA(error, maxAA, engine, base_thrust, wheels_ratio, error_rate=0, error_time=0):
         at_pid = PID2(1, 0.0, 1, 0, np.pi*10)
-        av_pid = PID(1, 0.0, 1, -1000, 1000)
+        av_pid = PID3(1, 0.0, 1, -1, 1, 3*dt)
         if wheels_ratio < 1:
             MoI = engine.maxThrust*lever*2/maxAA/(1-wheels_ratio)*base_thrust
             wheels_torque = maxAA * wheels_ratio * MoI
@@ -459,14 +512,16 @@ if __name__ == '__main__':
                   base_thrust,
                   tune_steering, wheels_torque)
         if error_rate > 0:
+            if error_time > 0:
+                return atc.simulate_random_attitude(error, error_rate, error_time, clampL(error * 2, 60))
             return atc.simulate_linear_attitude(error, error_rate, clampL(error * 2, 60))
         return atc.simulate_static_attitude(error, clampL(error * 2, 60))
 
-    def simAngle(eng, AA, base_thrust, wheels_ratio, error_rate, angles):
-        cols = 1 if error_rate > 0 else len(angles)
+    def simAngle(eng, AA, base_thrust, wheels_ratio, error_rate, error_time, angles):
         if error_rate > 0:
-            ATC.analyze_results(cols, 1, *[simAA(0, aa, eng, base_thrust, wheels_ratio, error_rate) for aa in AA])
+            ATC.analyze_results(1, 1, *[simAA(0, aa, eng, base_thrust, wheels_ratio, error_rate, error_time) for aa in AA])
         else:
+            cols = len(angles)
             for c, ang in enumerate(angles):
                 ATC.analyze_results(cols, c + 1, *[simAA(ang, aa, eng, base_thrust, wheels_ratio) for aa in AA])
         fig = plt.gcf()
@@ -476,10 +531,11 @@ if __name__ == '__main__':
     wheesly = Engine.from_file(datafile('Squad/Parts/Engine/jetEngines/jetEngineBasic.cfg'))
     LV_T30 = Engine.from_file(datafile('Squad/Parts/Engine/liquidEngineLV-T30/liquidEngineLV-T30.cfg'))
 
+    # np.random.seed(1)
     wheesly.acceleration /= 2
     wheesly.deceleration /= 2
     # simAngle(wheesly, (0.2, 1, 2, 9, 19), 0.7, 0.01, 0, (45, 15, 3))
-    simAngle(wheesly, AAs, 0.7, wheels_ratio, 0, angles)
+    simAngle(wheesly, AAs, 0.7, wheels_ratio, 10, 2, angles)
     # simAngle(wheesly, (0.5, 0.7, 0.9), 0.7, 0.02, 0, (45, 15, 3))
     # simAngle(wheesly, (0.5, 1, 2, 5, 10, 19), 0.7, 0.02, 0, (45, 15, 3))
     # simAngle(LV_T30, (0.009, 0.03, 0.1, 0.9, 1, 2, 10, 20), 1, 0.02, 0, (175, 60, 15, 3))
