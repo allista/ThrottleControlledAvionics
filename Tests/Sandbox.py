90d4baad48e5158e0f7afe684e655c52db88c2b0
player <allista@gmail.com>
2017-08-26 19:20:59 +0300

    Sandbox now draws grid in analyze_results.
    Also added constants to it.

--- Tests/Sandbox.py
+++ Tests/Sandbox.py
@@ -7,6 +7,8 @@ from common import color_grad
 class Sandbox(object):
     G = 9.80665
     twoPi = np.pi * 2
+    tenth_deg = 0.1 / 180 * np.pi
+    rad2deg = 180/np.pi
 
     class ZeroStats(object):
         def __init__(self, time, speed, desc, units):
@@ -20,9 +22,16 @@ class Sandbox(object):
             return ('Zero at: %f s\n'
                     '%s: %f %s' % (self.time, self.desc, self.speed, self.units))
 
+    @staticmethod
+    def _draw_grid(color):
+        plt.grid(b=False, which='major', axis='x', color=color, linestyle='-')
+        plt.grid(b=False, which='minor', axis='x', color='0.15', linestyle='--')
+        plt.grid(b=False, which='major', axis='y', color=color, linestyle='-')
+
     @classmethod
     def analyze_results(cls, cols, col, *results):
-        colors = color_grad(len(results))
+        colors = color_grad(len(results)+1)
+        gcolor = colors[-1]
         for i, result in enumerate(results):
             name, time, error, action, addons, zero_stats = result
             if zero_stats:
@@ -31,15 +40,17 @@ class Sandbox(object):
             nplots = 2+len(addons)
             ax = plt.subplot(nplots, cols, col)
             plt.plot(time, error, label=name, color=colors[i])
+            cls._draw_grid(gcolor)
             plt.ylabel('error')
             plt.legend()
             plt.subplot(nplots, cols, cols+col, sharex=ax)
             plt.plot(time, action, color=colors[i])
+            cls._draw_grid(gcolor)
             plt.ylabel('action')
-            plt.subplot(nplots, cols, cols*2+col, sharex=ax)
             if addons:
                 for curve, ylab in addons:
                     plt.plot(time, curve, color=colors[i])
+                    cls._draw_grid(gcolor)
                     plt.ylabel(ylab)
             plt.xlabel('time')
 
