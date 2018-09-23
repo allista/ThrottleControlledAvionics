0090ac076d96011de333176af65f93d6390e0e77
Allis Tauri <allista@gmail.com>
2018-09-23 12:19:17 +0300

    Reformatted AdvancedTab.cs, removed redundant 'using'

--- GUI/Tabs/AdvancedTab.cs
+++ GUI/Tabs/AdvancedTab.cs
@@ -7,7 +7,6 @@
 
 using System;
 using System.Linq;
-using System.Collections.Generic;
 using UnityEngine;
 using AT_Utils;
 
@@ -15,7 +14,7 @@ namespace ThrottleControlledAvionics
 {
     public class AdvancedTab : ControlTab
     {
-        public AdvancedTab(ModuleTCA tca) : base(tca) {}
+        public AdvancedTab(ModuleTCA tca) : base(tca) { }
 
         public override bool Valid { get { return true; } }
 
@@ -34,20 +33,20 @@ namespace ThrottleControlledAvionics
 
         #region Configs Selector
         public void UpdateNamedConfigs()
-        { 
+        {
             var configs = TCAScenario.NamedConfigs.Keys.ToList();
             var first = named_configs.Items.Count == 0;
-            configs.Add(string.Empty); named_configs.Items = configs; 
-            if(first) named_configs.SelectItem(configs.Count-1);
+            configs.Add(string.Empty); named_configs.Items = configs;
+            if(first) named_configs.SelectItem(configs.Count - 1);
         }
 
-        void SelectConfig_start() 
-        { 
-            named_configs.styleListBox  = Styles.list_box;
+        void SelectConfig_start()
+        {
+            named_configs.styleListBox = Styles.list_box;
             named_configs.styleListItem = Styles.list_item;
-            named_configs.windowRect    = UI.WindowPos;
+            named_configs.windowRect = UI.WindowPos;
             if(TCAScenario.NamedConfigs.Count > 0)
-                named_configs.DrawBlockingSelector(); 
+                named_configs.DrawBlockingSelector();
         }
 
         void SelectConfig()
@@ -61,7 +60,7 @@ namespace ThrottleControlledAvionics
                 if(new_config != selected_config)
                 {
                     selected_config = new_config;
-                    config_name = selected_config != null? selected_config.Name : string.Empty;
+                    config_name = selected_config != null ? selected_config.Name : string.Empty;
                 }
             }
         }
@@ -77,35 +76,35 @@ namespace ThrottleControlledAvionics
         public void Toggles()
         {
             GUILayout.BeginHorizontal();
-            if(VTOL != null) 
+            if(VTOL != null)
             {
-                if(Utils.ButtonSwitch("VTOL Mode", CFG.CTRL[ControlMode.VTOL], 
+                if(Utils.ButtonSwitch("VTOL Mode", CFG.CTRL[ControlMode.VTOL],
                                       "Keyboard controls thrust direction instead of torque", GUILayout.ExpandWidth(true)))
                     CFG.CTRL.XToggle(ControlMode.VTOL);
             }
-            if(VLA != null) 
-                Utils.ButtonSwitch("VTOL Assist", ref CFG.VTOLAssistON, 
+            if(VLA != null)
+                Utils.ButtonSwitch("VTOL Assist", ref CFG.VTOLAssistON,
                                    "Assist with vertical takeoff and landing", GUILayout.ExpandWidth(true));
-            if(STB != null) 
-                Utils.ButtonSwitch("Stabilizer", ref CFG.StabilizeFlight, 
+            if(STB != null)
+                Utils.ButtonSwitch("Stabilizer", ref CFG.StabilizeFlight,
                                    "Try to stabilize flight if spinning uncontrollably", GUILayout.ExpandWidth(true));
-            if(TRA != null) 
-                Utils.ButtonSwitch("H-Translation", ref CFG.CorrectWithTranslation, 
+            if(TRA != null)
+                Utils.ButtonSwitch("H-Translation", ref CFG.CorrectWithTranslation,
                                    "Use translation to correct horizontal velocity", GUILayout.ExpandWidth(true));
-            if(CPS != null) 
-                Utils.ButtonSwitch("CPS", ref CFG.UseCPS, 
+            if(CPS != null)
+                Utils.ButtonSwitch("CPS", ref CFG.UseCPS,
                                    "Enable Collistion Prevention System", GUILayout.ExpandWidth(true));
             GUILayout.EndHorizontal();
             GUILayout.BeginHorizontal();
-            Utils.ButtonSwitch("AutoGear", ref CFG.AutoGear, 
+            Utils.ButtonSwitch("AutoGear", ref CFG.AutoGear,
                                "Automatically deploy/retract landing gear when needed", GUILayout.ExpandWidth(true));
-            Utils.ButtonSwitch("AutoBrakes", ref CFG.AutoBrakes, 
+            Utils.ButtonSwitch("AutoBrakes", ref CFG.AutoBrakes,
                                "Automatically ebable/disable brakes when needed", GUILayout.ExpandWidth(true));
-            Utils.ButtonSwitch("AutoStage", ref CFG.AutoStage, 
+            Utils.ButtonSwitch("AutoStage", ref CFG.AutoStage,
                                "Automatically activate next stage when previous falmeouted", GUILayout.ExpandWidth(true));
-            Utils.ButtonSwitch("AutoChute", ref CFG.AutoParachutes, 
+            Utils.ButtonSwitch("AutoChute", ref CFG.AutoParachutes,
                                "Automatically activate parachutes when needed", GUILayout.ExpandWidth(true));
-            if(GUILayout.Button(new GUIContent("Modules", "Show TCA modules installed on this ship"), 
+            if(GUILayout.Button(new GUIContent("Modules", "Show TCA modules installed on this ship"),
                                 Styles.active_button, GUILayout.ExpandWidth(true)))
                 UI.ModulesGraph.Toggle();
             GUILayout.EndHorizontal();
@@ -120,13 +119,13 @@ namespace ThrottleControlledAvionics
             config_name = GUILayout.TextField(config_name, GUILayout.ExpandWidth(true), GUILayout.MinWidth(50));
             if(TCAScenario.NamedConfigs.ContainsKey(config_name))
             {
-                if(GUILayout.Button(new GUIContent("Overwrite", "Overwrite selected configuration with the current one"), 
+                if(GUILayout.Button(new GUIContent("Overwrite", "Overwrite selected configuration with the current one"),
                                     Styles.danger_button, GUILayout.ExpandWidth(false)))
                     TCAScenario.SaveNamedConfig(config_name, CFG, true);
             }
-            else if(GUILayout.Button(new GUIContent("Add", "Save current configuration"), 
-                                     Styles.add_button, GUILayout.ExpandWidth(false)) 
-                    && config_name != string.Empty) 
+            else if(GUILayout.Button(new GUIContent("Add", "Save current configuration"),
+                                     Styles.add_button, GUILayout.ExpandWidth(false))
+                    && config_name != string.Empty)
             {
                 TCAScenario.SaveNamedConfig(config_name, CFG);
                 UpdateNamedConfigs();
@@ -134,15 +133,15 @@ namespace ThrottleControlledAvionics
             }
             SelectConfig();
             if(GUILayout.Button(new GUIContent("Load", "Load selected configuration"),
-                                Styles.active_button, GUILayout.ExpandWidth(false)) 
-               && selected_config != null) 
-                CFG.Copy(selected_config);
-            if(GUILayout.Button(new GUIContent("Delete", "Delete selected configuration"), 
-                                Styles.danger_button, GUILayout.ExpandWidth(false)) 
+                                Styles.active_button, GUILayout.ExpandWidth(false))
                && selected_config != null)
-            { 
+                CFG.Copy(selected_config);
+            if(GUILayout.Button(new GUIContent("Delete", "Delete selected configuration"),
+                                Styles.danger_button, GUILayout.ExpandWidth(false))
+               && selected_config != null)
+            {
                 TCAScenario.NamedConfigs.Remove(selected_config.Name);
-                named_configs.SelectItem(named_configs.SelectedIndex-1);
+                named_configs.SelectItem(named_configs.SelectedIndex - 1);
                 UpdateNamedConfigs();
                 selected_config = null;
             }
@@ -181,27 +180,27 @@ namespace ThrottleControlledAvionics
             GUILayout.Label(UI.Title, Styles.label, GUILayout.ExpandWidth(true));
             GUILayout.BeginHorizontal();
             //change key binding
-            if(GUILayout.Button(SelectingKey? new GUIContent("HotKey: ?", "Choose new TCA hotkey") : 
-                                new GUIContent(string.Format("HotKey: {0}", UI.TCA_Key), "Select TCA Hotkey"), 
-                                SelectingKey? Styles.enabled_button : Styles.active_button, 
+            if(GUILayout.Button(SelectingKey ? new GUIContent("HotKey: ?", "Choose new TCA hotkey") :
+                                new GUIContent(string.Format("HotKey: {0}", UI.TCA_Key), "Select TCA Hotkey"),
+                                SelectingKey ? Styles.enabled_button : Styles.active_button,
                                 GUILayout.ExpandWidth(true)))
-            { 
-                SelectingKey = true; 
+            {
+                SelectingKey = true;
                 Utils.Message("Press a key that will toggle TCA.\n" +
                               "Press BACKSPACE to remove TCA hotkey.\n" +
-                              "Press ESCAPE to cancel."); 
+                              "Press ESCAPE to cancel.");
             }
-            Utils.ButtonSwitch("AutoSave", ref Globals.Instance.AutosaveBeforeLanding, 
-                               "Automatically save the game before executing complex autopilot programs", 
+            Utils.ButtonSwitch("AutoSave", ref Globals.Instance.AutosaveBeforeLanding,
+                               "Automatically save the game before executing complex autopilot programs",
                                GUILayout.ExpandWidth(true));
-            if(Utils.ButtonSwitch(Globals.Instance.UseStockAppLauncher? "Launcher" : "Toolbar", 
+            if(Utils.ButtonSwitch(Globals.Instance.UseStockAppLauncher ? "Launcher" : "Toolbar",
                                   ref Globals.Instance.UseStockAppLauncher,
                                   "Use stock AppLauncher or Toolbar plugin?",
                                   GUILayout.ExpandWidth(true)))
                 TCAToolbarManager.Init();
-            Utils.ButtonSwitch("AutoShow", ref TCAGui.Instance.ShowOnHover, 
+            Utils.ButtonSwitch("AutoShow", ref TCAGui.Instance.ShowOnHover,
                                "Show collapsed TCA window when mouse hovers over it", GUILayout.ExpandWidth(true));
-            if(GUILayout.Button(new GUIContent("Reload", "Reload TCA settings from file"), 
+            if(GUILayout.Button(new GUIContent("Reload", "Reload TCA settings from file"),
                                 Styles.active_button, GUILayout.ExpandWidth(true)))
             {
                 Globals.Load();
@@ -240,13 +239,17 @@ namespace ThrottleControlledAvionics
                     if(e.keyCode == KeyCode.None && char.IsLetterOrDigit(e.character))
                     {
                         var ec = new string(e.character, 1).ToUpper();
-                        if(char.IsDigit(e.character)) ec = "Alpha"+ec;
+                        if(char.IsDigit(e.character)) ec = "Alpha" + ec;
                         try { e.keyCode = (KeyCode)Enum.Parse(typeof(KeyCode), ec); }
-                        catch(Exception ex) { Utils.Log("TCA GUI: exception caught while trying to set hotkey:\n{}", ex); }
+                        catch(Exception ex)
+                        { Utils.Log("TCA GUI: exception caught while trying to set hotkey:\n{}", ex); }
                     }
-                    if(e.keyCode == KeyCode.None) 
-                        Utils.Message("Unable to convert '{0}' to keycode.\nPlease, try an alphabet character.", e.character);
-                    else UI.TCA_Key = e.keyCode;
+                    if(e.keyCode == KeyCode.None)
+                        Utils.Message("Unable to convert '{0}' to keycode.\n" +
+                                      "Please, try an alphabet or numeric character.",
+                                      e.character);
+                    else
+                        UI.TCA_Key = e.keyCode;
                     Utils.Log("TCA: new key slected: {}", UI.TCA_Key);
                 }
                 SelectingKey = false;
