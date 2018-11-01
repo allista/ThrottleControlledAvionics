e1f7a647a8ccb647a7f433eca01de9765eeac2ee
Allis Tauri <allista@gmail.com>
2018-11-01 22:27:39 +0300

    fixup! Reformatted landingTrajector.cs

--- Modules/Orbit/Trajectories/LandingTrajectory.cs
+++ Modules/Orbit/Trajectories/LandingTrajectory.cs
@@ -500,9 +500,7 @@ namespace ThrottleControlledAvionics
         public List<Point> Points = new List<Point>();
 
         public Point LastPoint { get; private set; }
-
         public bool HavePoints { get; private set; }
-
         public bool Atmosphere { get; private set; }
         public int Count => Points.Count;
 
