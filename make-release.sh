e1f988808389873c775d027e5576f46cc48df1f1
Allis Tauri <allista@gmail.com>
2019-11-14 17:26:22 +0300

    Also exclude UnityEngine.*.dlls from release archive

--- make-release.sh
+++ make-release.sh
@@ -4,7 +4,7 @@ cd $(dirname "$0")
 
 ../../PyKSPutils/make_mod_release \
 -e '*/config.xml' '*/TCA.conf' '*/TCA.macro' '*.user' '*.orig' '*.mdb' '*.pdb' \
-'*/System.*.dll' '*/Mono.*.dll' '*/Unity.*.dll' \
+'*/System.*.dll' '*/Mono.*.dll' '*/Unity*.dll' \
 '*/001_AnisotropicPartResizer*' \
 '*/002_MultiAnimators.dll' \
 '*/AnimatedConverters.dll' \
@@ -12,4 +12,3 @@ cd $(dirname "$0")
 '*/SubmodelResizer.dll' \
 'GameData/000_AT_Utils/ResourceHack.cfg' \
 -i '../AT_Utils/GameData'
-
