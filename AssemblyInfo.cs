c731787a023b5a9167252d21a45e07fbcb2eeaf6
Allis Tauri <allista@gmail.com>
2020-06-22 20:22:05 +0300

    AssemblyVersion

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -28,11 +28,11 @@ using AT_Utils;
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
 #if NIGHTBUILD
-[assembly: AssemblyVersion("3.6.*")]
+[assembly: AssemblyVersion("3.7.*")]
 #else
-[assembly: AssemblyVersion("3.6.2.2")]
+[assembly: AssemblyVersion("3.7.0")]
 #endif
-[assembly: KSPAssembly("ThrottleControlledAvionics", 3, 6)]
+[assembly: KSPAssembly("ThrottleControlledAvionics", 3, 7)]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
