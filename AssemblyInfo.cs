af726b03d4e970d9d99f64fd1195b3afd41ac03e
Allis Tauri <allista@gmail.com>
2020-07-05 14:10:15 +0300

    AssemblyVersion

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -30,7 +30,7 @@ using AT_Utils;
 #if NIGHTBUILD
 [assembly: AssemblyVersion("3.7.*")]
 #else
-[assembly: AssemblyVersion("3.7.0")]
+[assembly: AssemblyVersion("3.7.0.1")]
 #endif
 [assembly: KSPAssembly("ThrottleControlledAvionics", 3, 7)]
 
@@ -47,7 +47,7 @@ namespace ThrottleControlledAvionics
         public ModInfo()
         {
             MinKSPVersion = new Version(1, 9, 0);
-            MaxKSPVersion = new Version(1, 9, 1);
+            MaxKSPVersion = new Version(1, 10, 0);
 
             VersionURL = "https://raw.githubusercontent.com/allista/ThrottleControlledAvionics/master/GameData/ThrottleControlledAvionics/ThrottleControlledAvionics.version";
             UpgradeURL = "http://spacedock.info/mod/198/Throttle%20Controlled%20Avionics";
